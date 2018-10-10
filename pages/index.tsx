052766a93fb6dcc8f55de8e44ca22229a9fa7dd3
Igor Kamyshev <igor@kamyshev.me>
2018-10-10 10:30:42 +0300

    Added component for Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,21 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { AnyAction, Dispatch } from 'redux'
 
-import Examples, { actions } from '@app/features/examples'
+import LandingPage from '@app/features/landing'
 import { AppContext } from '@app/lib/server-types'
 
-interface Props {
-  start(): NodeJS.Timer
-}
+class Index extends React.Component {
 
-class Index extends React.Component<Props> {
-
-  public static getInitialProps({ reduxStore, req }: AppContext) {
-    const isServer = !!req
-    reduxStore.dispatch(actions.serverRenderClock(isServer) as any)
+  public static getInitialProps(context: AppContext) {
+    // prepare data for landing
 
     return {}
-  }
-  private timer?: NodeJS.Timer
-
-  public componentDidMount() {
-    this.timer = this.props.start()
-  }
-
-  public componentWillUnmount() {
-    clearInterval(this.timer!)
   }
 
   public render() {
     return (
-      <Examples />
+      <LandingPage />
     )
   }
 }
 
-export default connect(
-  null,
-  (dispatch: Dispatch<AnyAction>) => ({
-    start: () => actions.startClock(dispatch) as any,
-  }),
-)(Index)
+export default Index
