c6af32f609f1b289b8eed6b1ba645eb9514c4d93
iwanttobealight <iwanttobealight@yandex.ru>
2018-10-26 17:05:50 +0300

    add createQuotaForm

---
+++ features/createQuota/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../layout'
+import Form from './organisms/Form'
+
+class CreateQuota extends React.Component {
+  public render() {
+    return (
+      <Layout>
+        <Form />
+      </Layout>
+    )
+  }
+}
+
+export default CreateQuota
---
+++ features/createQuota/organisms/Form/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as styles from './Form.css'
+
+const Form = () => (
+  <div className={styles.container}>create quota form</div>
+)
+export default Form
