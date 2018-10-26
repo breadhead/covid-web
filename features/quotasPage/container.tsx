f52421e273c11d0012ad10989f028d6d5056e8c4
uncleseneca <uncleseneca@gmail.com>
2018-10-26 10:57:59 +0300

    fixed imports

--- features/quotasPage/container.tsx
+++ features/quotasPage/container.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { AppContext, ExpressError } from '@app/lib/server-types'
-import { handleAnonymous } from '../login'
+import { handleUnauthorized } from '../login'
 import { fetchQuotas } from './actions'
 import QuotasPage from './page'
 
@@ -10,7 +10,7 @@ class Container extends React.Component {
   public static async getInitialProps(context: AppContext) {
     await context.reduxStore
       .dispatch(fetchQuotas() as any)
-      .catch((error: ExpressError) => handleAnonymous(error, context.res))
+      .catch((error: ExpressError) => handleUnauthorized(error, context.res))
     return {}
   }
 
