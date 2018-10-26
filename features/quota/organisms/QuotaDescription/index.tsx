72f1859c4978ec61b440ddc30dbba5043b06a03d
maryns0n <zogacc@gmail.com>
2018-10-26 16:10:43 +0300

    add organism

---
+++ features/quota/organisms/QuotaDescription/index.tsx
@@ -0,0 +1,27 @@
+import { Col, Icon, Row } from 'antd'
+import * as React from 'react'
+
+import CompanyLogo from '@app/ui/molecules/CompanyLogo'
+import QuotaDetails from '@app/ui/molecules/QuotaDetails'
+import IsCompaniyPublic from '../../molecules/IsCompanyPublic'
+import QuotaConstraints from '../../molecules/QuotaConstraints'
+import styles from './QuotaDescription.css'
+
+const QuotaDescription = ({ quota }: Props) => (
+  <div className={styles.QuotaDescription}>
+    <QuotaConstraints constraints={quota.constraints} />
+    <QuotaDetails name={quota.company.name} type={quota.type} comment={quota.comment} />
+    <IsCompaniyPublic publicCompany={quota.publicCompany}/>
+    <Row gutter={16}>
+      <Col span={3}>
+        <CompanyLogo logo={quota.company.logo} site={quota.company.site} />
+      </Col>
+      <Col span={8}>
+        {/* TODO: Change a public comment when the API is completed. */}
+        <span>{quota.company.publicComment}</span>
+      </Col>
+    </Row>
+  </div>
+)
+
+export default QuotaDescription
