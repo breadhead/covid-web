3397dba4cdf235a021db1bf96364ef6d2707ca34
maryns0n <zogacc@gmail.com>
2018-10-26 16:10:53 +0300

    add organism

---
+++ features/quota/organisms/QuotaControl/index.tsx
@@ -0,0 +1,48 @@
+import { Button, Card, Col, Form, Input as AntInput, Row } from 'antd'
+import * as React from 'react'
+
+import Input from '@app/ui/molecules/Input'
+import styles from './QuotaControl.css'
+
+const FormItem = Form.Item
+
+const QuotaControl = ({ quota }: Props) => (
+
+  <Card>
+    <Row type="flex" aling="center" gutter={24}>
+      <Col span={2} className={styles.QuotaAmount}>
+        <h4>Всего</h4>
+        <strong>{quota.company.donation}</strong>
+      </Col>
+      <Col span={2} className={styles.QuotaAmount}>
+        <h4>Доступно</h4>
+        <strong className={styles.QuotaAvailable}>{quota.count}</strong>
+      </Col>
+      <Col span={12} offset={7}>
+        <Form
+          className={styles.Form}
+          layout="inline"
+          onSubmit={}
+        >
+          <FormItem>
+            <Input
+              type="number"
+              size="large"
+              label="Количество квот" />
+          </FormItem>
+          <FormItem>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+            >
+              Добавить
+            </Button>
+          </FormItem>
+        </Form>
+      </Col>
+    </Row>
+  </Card>
+)
+
+export default QuotaControl
