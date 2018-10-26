8bccbcc3b61149818e085ffcf42334614a78fbc2
maryns0n <zogacc@gmail.com>
2018-10-26 16:09:28 +0300

    add molecula

---
+++ ui/molecules/QuotaDetails/index.tsx
@@ -0,0 +1,21 @@
+import { Col, Row } from 'antd'
+import * as React from 'react'
+
+import styles from './QuotaDetails.css'
+
+const QuotaDetails = ({ type, name, comment }: Props) =>
+  <p className={styles.QuotaDetails}>
+    <Row gutter={16}>
+      <Col span={2}>
+        <strong>{type}</strong>
+      </Col>
+      <Col span={2}>
+        <span>{name}</span>
+      </Col>
+      <Col span={20}>
+        <span>({comment})</span>
+      </Col>
+    </Row>
+  </p>
+
+export default QuotaDetails
