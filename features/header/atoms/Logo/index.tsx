4a48d3cd4c53d6ee20ec6b2e765c0c93711454f3
uncleseneca <uncleseneca@gmail.com>
2018-10-26 17:42:08 +0300

    fixed linter errors

--- features/header/atoms/Logo/index.tsx
+++ features/header/atoms/Logo/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './Logo.css'
-const Logo = () => <div className={styles.Logo}>Logo</div>
+const Logo = () => <div>Logo</div>
 
 export default Logo
--- features/quotasPage/organisms/QuotaCard/atoms/CompanyName/index.tsx
+++ features/quotasPage/organisms/QuotaCard/atoms/CompanyName/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import styles from './CompanyName.css'
 
 export interface CompanyNameProps {
   children: string
 }
 
 const CompanyName: React.SFC<CompanyNameProps> = ({ children }) => {
-  return (<div className={styles.CompanyName}>{children}</div>)
+  return (<div>{children}</div>)
 }
 
 export default CompanyName
