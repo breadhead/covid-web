d0927e8a19e932367a688d81964dcb95b30d3862
iwanttobealight <iwanttobealight@yandex.ru>
2018-10-26 17:52:16 +0300

    add create-new-quota link

--- features/header/molecules/Navigation/index.tsx
+++ features/header/molecules/Navigation/index.tsx
@@ -5,6 +5,7 @@ import styles from './Navigation.css'
 const Navigation = () => <nav className={styles.Navigation}>
   <NavLink href="/quotas"><a>Квоты</a></NavLink>
   <NavLink href="/login"><a>Логин</a></NavLink>
+  <NavLink href="/create-new-auota"><a>Создать квоту</a></NavLink>
 </nav>
 
 export default Navigation
--- features/landing/page/index.tsx
+++ features/landing/page/index.tsx
@@ -7,6 +7,8 @@ const LandingPage = () => (
     <Link href="/login"><a>login</a></Link>
     <div></div>
     <Link href="/quotas"><a>quotas</a></Link>
+    <div></div>
+    <Link href="/create-new-quota"><a>create new quota</a></Link>
   </main>
 )
 
