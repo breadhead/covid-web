be5c7fc4b1c7c1402221160c99b56c00c4e1c5d2
uncleseneca <uncleseneca@gmail.com>
2018-10-26 14:06:29 +0300

    moved components from header to molecules

--- features/header/organisms/Header/index.tsx
+++ features/header/organisms/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Logo from '../Logo'
-import Navigation from '../Navigation'
-import User from '../User'
+import Logo from '../../atoms/Logo'
+import User from '../../atoms/User'
+import Navigation from '../../molecules/Navigation'
 
 const Header = () =>
   <header>
