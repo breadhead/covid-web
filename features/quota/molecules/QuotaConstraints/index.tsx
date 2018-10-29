7a9fd5e39e8b241f1f567b3ed404bb621d124dbb
maryns0n <zogacc@gmail.com>
2018-10-29 13:59:18 +0300

    fix tslint issues

--- features/quota/molecules/QuotaConstraints/index.tsx
+++ features/quota/molecules/QuotaConstraints/index.tsx
@@ -3,25 +3,19 @@ import * as React from 'react'
 import styles from './QuotaConstraints.css'
 
 class QuotaConstraints extends React.Component {
-  constructor() {
-    super()
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  public handleClick() {
-    // TODO: add popup showing on click after popup availability
-  }
 
   public render() {
-    return(
+    return (
       <p className={styles.QuotaConstraints}>
         {this.constraints + ' '}
         {/* TODO: change icon by button after UI-kit availability */}
-        <Icon type="edit" theme="filled" onClick={this.handleClick}/>
+        <Icon type="edit" theme="filled" onClick={this.handleClick} />
       </p>
     )
   }
+
+  private handeleClick = () =>
+    // TODO: add popup showing on click after popup availability
 }
 
 export default QuotaConstraints
