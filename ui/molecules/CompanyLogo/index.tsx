ce5b3b4b0c3271aabc8eaffc225a62f0888be4ad
maryns0n <zogacc@gmail.com>
2018-10-29 15:15:22 +0300

    wrap logo in component for external links

--- ui/molecules/CompanyLogo/index.tsx
+++ ui/molecules/CompanyLogo/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react'
 
+import ExternalLink from '@app/ui/molecules/ExternalLink'
 import styles from './CompanyLogo.css'
 
 const CompanyLogo = ({ logo, site }) => {
   return site === ''
   ? <img src={logo} className={styles.CompanyLogo}/>
   : (
-      <a href={site}>
-        <img src={logo} className={styles.CompanyLogo}/>
-      </a>
+      <ExternalLink href={site}>
+        <img src={logo} className={styles.CompanyLogo} />
+      </ExternalLink>
   )
 }
 
