ad36a3044844351ae3b49c0a2061c1f13b59597d
maryns0n <zogacc@gmail.com>
2018-10-29 16:02:58 +0300

    remove AntDesign's rows and cols

--- features/quota/organisms/QuotaControl/index.tsx
+++ features/quota/organisms/QuotaControl/index.tsx
@@ -9,42 +9,40 @@ const FormItem = Form.Item
 
 const QuotaControl = ({ quota }: Quota) => (
 
-  <Card>
-    <Row type="flex" aling="center" gutter={24}>
-      <Col span={2} className={styles.QuotaAmount}>
-        <h4>Всего</h4>
-        <strong>{quota.company.donation}</strong>
-      </Col>
-      <Col span={2} className={styles.QuotaAmount}>
-        <h4>Доступно</h4>
-        <strong className={styles.QuotaAvailable}>{quota.count}</strong>
-      </Col>
-      <Col span={12} offset={7}>
-        <Form
-          className={styles.Form}
-          layout="inline"
-          // TODO: add quota accrual process on submit
-          // onSubmit={}
-        >
-          <FormItem>
-            <Input
-              type="number"
-              size="large"
-              label="Количество квот" />
-          </FormItem>
-          <FormItem>
-            <Button
-              type="primary"
-              htmlType="submit"
-              size="large"
-            >
-              Добавить
-            </Button>
-          </FormItem>
-        </Form>
-      </Col>
-    </Row>
-  </Card>
+  <div className={styles.QuotaControl}>
+    <div span={3} className={styles.QuotaAmount}>
+      <h4>Всего</h4>
+      <strong>{quota.company.donation}</strong>
+    </div>
+    <div span={3} className={styles.QuotaAmount}>
+      <h4>Доступно</h4>
+      <strong className={styles.QuotaAvailable}>{quota.count}</strong>
+    </div>
+    <div className={styles.AddingInput}>
+      <Form
+        className={styles.Form}
+        layout="inline"
+        // TODO: add quota accrual process on submit
+        // onSubmit={}
+      >
+        <FormItem>
+          <Input
+            type="number"
+            size="large"
+            label="Количество квот" />
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+          >
+            Добавить
+          </Button>
+        </FormItem>
+      </Form>
+    </div>
+  </div>
 )
 
 export default QuotaControl
--- ui/molecules/QuotaDetails/index.tsx
+++ ui/molecules/QuotaDetails/index.tsx
@@ -5,17 +5,9 @@ import styles from './QuotaDetails.css'
 
 const QuotaDetails = ({ type, name, comment }) =>
   <p className={styles.QuotaDetails}>
-    <Row gutter={16}>
-      <Col span={2}>
-        <strong>{type}</strong>
-      </Col>
-      <Col span={2}>
-        <span>{name}</span>
-      </Col>
-      <Col span={20}>
-        <span>({comment})</span>
-      </Col>
-    </Row>
+    <strong>{type}</strong>
+    <span>{name}</span>
+    <span>({comment})</span>
   </p>
 
 export default QuotaDetails
