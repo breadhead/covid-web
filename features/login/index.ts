0f18854c0885af6c808ec56383e183a1c48c1c2f
uncleseneca <uncleseneca@gmail.com>
2018-10-29 17:23:53 +0300

    moved loginPage to login feature

--- features/login/index.ts
+++ features/login/index.ts
@@ -1,8 +1,7 @@
-import Container from './container'
-import Form from './organisms/Form'
-export default Container(Form)
 
 export { reducer, State } from './reducer'
 export { login } from './actions'
 export { handleUnauthorized } from './helpers/handleUnauthorized'
 export { throwAuthErrorFurther } from './helpers/throwAuthErrorFurther'
+
+export { default as default } from './page'
