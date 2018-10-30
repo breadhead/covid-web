09b7c9177b8a5dc0b77a95e77a8bff596eef8e2d
uncleseneca <uncleseneca@gmail.com>
2018-10-30 16:31:53 +0300

    added tsoption to getQuotacount

--- features/transfer/helpers/getQuotaCount.ts
+++ features/transfer/helpers/getQuotaCount.ts
@@ -1,9 +1,9 @@
-import { none, Option, option, some } from 'ts-option'
+import { Option } from 'tsoption'
 import { StrippedQuota } from '../container'
 export const getQuotaCount = (
   id: string,
-  quotas: StrippedQuota[]) => {
-  const foundQuota = quotas.find((quota) => quota.id === id)
-  const foundQuotaCount = foundQuota ? foundQuota.count : 0
-  return foundQuotaCount
-}
+  quotas: StrippedQuota[]) => Option
+    .of(quotas.find((quota) => quota.id === id))
+    .map((quota) => quota.count)
+    .map((count) => parseInt(count, 10))
+    .getOrElse(0)
