b7aee18d4b153df44673a03d115af830c41a1eb1
maryns0n <zogacc@gmail.com>
2018-10-30 14:35:34 +0300

    refactor component

--- ui/molecules/CompanyLogo/index.tsx
+++ ui/molecules/CompanyLogo/index.tsx
@@ -3,12 +3,21 @@ import * as React from 'react'
 import ExternalLink from '@app/ui/molecules/ExternalLink'
 import styles from './CompanyLogo.css'
 
-const CompanyLogo = ({ logo, site }) => {
-  return site === ''
-  ? <img src={logo} className={styles.CompanyLogo}/>
+interface Props {
+  logo: string,
+  site: string | null
+}
+
+const logoImg = (src: string) => (
+  <img src={src} className={styles.CompanyLogo} />
+)
+
+const CompanyLogo = ({ logo, site }: Props) => {
+  return !!site
+  ? logoImg(logo)
   : (
       <ExternalLink href={site}>
-        <img src={logo} className={styles.CompanyLogo} />
+        {logoImg(logo)}
       </ExternalLink>
   )
 }
