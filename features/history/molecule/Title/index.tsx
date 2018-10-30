a84fe41d298dd2441c4282807a63401b38973e7e
Igor Kamyshev <igor@kamyshev.me>
2018-10-30 18:01:58 +0300

    Add styling for range picker

--- features/history/molecule/Title/index.tsx
+++ features/history/molecule/Title/index.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 
-import * as styles from './Title.css'
-
 const Title = () => (
-  <h1 className={styles.Title}>История операций</h1>
+  <h1>История операций</h1>
 )
 
 export default Title
--- features/history/organisms/Header/index.tsx
+++ features/history/organisms/Header/index.tsx
@@ -1,8 +1,10 @@
+import { Col, Row } from 'antd'
 import * as React from 'react'
 
 import BackButton from '../../molecule/BackButton'
 import RangePicker from '../../molecule/RangePicker'
 import Title from '../../molecule/Title'
+import * as styles from './Header.css'
 
 interface Props {
   onChangePeriod: (from?: Date, to?: Date) => Promise<void>
@@ -11,8 +13,14 @@ interface Props {
 const Header = ({ onChangePeriod }: Props) => (
   <React.Fragment>
     <BackButton />
-    <Title />
-    <RangePicker onChange={onChangePeriod} />
+    <Row className={styles.Main}>
+      <Col span={18}>
+        <Title />
+      </Col>
+      <Col span={6}>
+        <RangePicker onChange={onChangePeriod} />
+      </Col>
+    </Row>
   </React.Fragment>
 )
 
