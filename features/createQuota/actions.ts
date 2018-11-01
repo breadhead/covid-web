0072e65163a670232a7de156922974deeeea83c3
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-01 15:38:22 +0300

    fix type

--- features/createQuota/actions.ts
+++ features/createQuota/actions.ts
@@ -1,8 +1,9 @@
 import { ExtraArgs, State } from '@app/lib/store'
 import { Dispatch } from 'redux'
+import { QuotaFields } from './organisms/Form'
 import { actions } from './reducer'
 
-export const createQuota = (quotaFields: any) => async (
+export const createQuota = (quotaFields: QuotaFields) => async (
   dispatch: Dispatch<any>,
   _: () => State,
   { api }: ExtraArgs,
