687480c29e0d37e19f2e9fa03fddf74cd6fe8c39
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-01 15:38:08 +0300

    fix quotatype

--- features/createQuota/atoms/Select/index.tsx
+++ features/createQuota/atoms/Select/index.tsx
@@ -1,20 +1,16 @@
 import { QuotaType } from '@app/models/Quota/Quota'
 import { default as UiSelect } from '@app/ui/molecules/Select'
 
-const QUOTA_TYPE = new Array()
-for (const key in QuotaType) { //tslint:disable-line
-  QUOTA_TYPE.push({ name: QuotaType[key], value: QuotaType[key] })
-}
-
-const Select = () => {
-  return (
-    <UiSelect
-      name="category"
-      label="Категория типа квот"
-      defaultValue="Common"
-      options={QUOTA_TYPE}
-    />
-  )
-}
+const Select = () => (
+  <UiSelect
+    name="category"
+    label="Категория типа квот"
+    defaultValue="Common"
+    options={Object
+      .entries(QuotaType)
+      .map(([_, value]) => ({ name: value, value }))
+    }
+  />
+)
 
 export default Select
