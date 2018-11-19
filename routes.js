c29eb744235b435c56c18ead2241fccfb9764fc2
uncleseneca <uncleseneca@gmail.com>
2018-11-19 18:58:39 +0300

    changed routes

--- routes.js
+++ routes.js
@@ -3,11 +3,11 @@ const nextRoutes = require('next-routes')
 
 module.exports = nextRoutes()
   // admin routes
-  .add('transfer', '/transfer', 'admin-transfer')
-  .add('quota', '/quota/:id', 'admin-quota')
-  .add('create-quota', '/create-quota', 'admin-create-quota')
-  .add('history', '/history', 'admin-history')
-  .add('login', '/login', 'admin-login')
-  .add('quotas', '/quotas', 'admin-quotas')
+  .add('transfer', '/admin/transfer', 'admin-transfer')
+  .add('quota', '/admin/quota/:id', 'admin-quota')
+  .add('create-quota', '/admin/create-quota', 'admin-create-quota')
+  .add('history', '/admin/history', 'admin-history')
+  .add('login', '/admin/login', 'admin-login')
+  .add('quotas', '/admin/quotas', 'admin-quotas')
   // main routes
   .add('home', '/', 'main-index')
