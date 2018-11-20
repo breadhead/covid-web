14552234927839aa00b86404dd824e323284d758
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-20 14:47:19 +0300

    update Switch

--- features/main/landing/page/index.tsx
+++ features/main/landing/page/index.tsx
@@ -3,18 +3,20 @@ import * as React from 'react'
 import { Form as AntForm } from 'antd'
 import { Form as FinalForm } from 'react-final-form'
 
+import Switch from '@app/ui/molecules/Switch'
 import TextArea from '@app/ui/molecules/TextАrea'
 
-import * as styles from './Test.css'
-
 const LandingPage = () => (
-  <main>
-    <h1 className={styles.title}>Oncohelp</h1>
+  <main
+    style={{ margin: '0 auto', maxWidth: '800px' }}
+  >
+    <h1>Oncohelp</h1>
     <FinalForm
       onSubmit={() => undefined}
       render={() => (
         <AntForm>
-          <TextArea name="test" />
+          <TextArea name="testTextArea" />
+          <Switch name="testSWitch" />
         </AntForm>
       )}
 
