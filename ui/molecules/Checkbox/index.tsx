5af21678f07088e3317fda20a98d8981ba701584
martyns0n <zogacc@gmail.com>
2018-11-21 16:21:39 +0300

    add ui states of checkbox

--- ui/molecules/Checkbox/index.tsx
+++ ui/molecules/Checkbox/index.tsx
@@ -1,22 +1,44 @@
-import { Checkbox as AntCheckbox } from 'antd'
+import { Checkbox as AntCheckbox, Form as AntForm } from 'antd'
 import * as React from 'react'
+import { Field as FinalField } from 'react-final-form'
+
 import './Checkbox.css?CSSModulesDisable'
 
-// interface Props {
+const FormItem = AntForm.Item
 
-// }
+interface Props {
+  name: string
+  value: string
+  className?: string
+  label?: string
+  children?: React.ReactNode
+  disabled?: boolean
+  className?: string
+  checked?: boolean
+  style?: React.CSSProperties
+}
 
 const Checkbox = ({
+  name,
+  value,
+  className,
+  label,
+  children,
   ...rest
-}) =>
-  <AntCheckbox
-    // className={styles.Checkbox}
-    // checked={checked}
-    // defaultChecked={decaultChecked}
-    // disabled={disabled}
-    // indeterminate={indeterminate}
-    // onChange={onChange}
-    {...rest}
-  />
+}: Props) =>
+  <FinalField className={className} name={name}>
+    {({ meta }) => (
+      <FormItem
+        validateStatus={meta.submitError && 'error'}
+        help={meta.submitError}
+      >
+        <AntCheckbox
+          {...rest}
+          >
+          {children}
+        </AntCheckbox>
+      </FormItem>
+    )}
+  </FinalField>
 
 export default Checkbox
