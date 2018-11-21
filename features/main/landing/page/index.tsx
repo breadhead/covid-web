c93276eec99d19466d294c43eb3b645907363e35
martyns0n <zogacc@gmail.com>
2018-11-21 13:03:59 +0300

    add component on test page

--- features/main/landing/page/index.tsx
+++ features/main/landing/page/index.tsx
@@ -3,18 +3,20 @@ import * as React from 'react'
 import { Form as AntForm } from 'antd'
 import { Form as FinalForm } from 'react-final-form'
 
+import Checkbox from '@app/ui/molecules/Checkbox'
 import TextArea from '@app/ui/molecules/TextАrea'
 
-import * as styles from './Test.css'
+// import * as styles from './Test.css'
 
 const LandingPage = () => (
   <main>
-    <h1 className={styles.title}>Oncohelp</h1>
+    <h1>Oncohelp</h1>
     <FinalForm
       onSubmit={() => undefined}
       render={() => (
         <AntForm>
           <TextArea name="test" />
+          <Checkbox />
         </AntForm>
       )}
 
