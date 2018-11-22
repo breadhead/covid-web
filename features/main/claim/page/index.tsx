0a14caf530f03b5327442ccfd71f1be30e6a8e5c
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-22 19:57:41 +0300

    add mobile styles for Claim page

--- features/main/claim/page/index.tsx
+++ features/main/claim/page/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
 import * as styles from './Claim.css'
 
-import ClaimForm from '../organisms/ClaimForm'
+import { NON_BREAKING_SPACE } from '@app/lib/config'
 
 const Claim = () => {
   return (
     <main className={styles.claimPage}>
       <h1 className={styles.title}>Заполните заявку</h1>
-      <p className={styles.infoText}>Личные данные будут использованы только для консультации.</p>
-      <ClaimForm />
+      <p className={styles.infoText}>
+      Личные данные будут использованы только для{NON_BREAKING_SPACE}консультации.
+      </p>
+      {/* <ClaimForm /> */}
     </main>
   )
 }
