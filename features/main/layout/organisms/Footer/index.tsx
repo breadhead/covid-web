2d4c7d55865e4089e4526e79736c4fc1e01a82fc
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-22 22:41:15 +0300

    add footer

---
+++ features/main/layout/organisms/Footer/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import * as styles from './Footer.css'
+
+import NavLink from '@app/ui/atoms/NavLink'
+import Button from '@app/ui/molecules/Button'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+
+      <div className={styles.row}>
+        <div className={styles.logo}>Просто спросить</div>
+        <nav className={styles.menu}>
+          <NavLink type="nav" className={styles.link}>Партнёры</NavLink>
+          <NavLink type="nav" className={styles.link}>Контакты</NavLink>
+          <NavLink type="nav" className={styles.link}>Помочь проекту</NavLink>
+          <NavLink type="nav" className={styles.link}>Обратная связь</NavLink>
+          <NavLink type="nav" className={styles.link}>Эксперты</NavLink>
+        </nav>
+        <div>
+          <Button kind="secondary">Войти</Button>
+          <Button kind="primary">Просто спросить</Button>
+        </div>
+      </div>
+
+      <div className={styles.row}>
+      
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
