90c5771edbf2b68aa1e780676d996f9d2c663510
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-23 17:59:09 +0300

    decompose BottomRow from footer

--- features/main/layout/organisms/Footer/index.tsx
+++ features/main/layout/organisms/Footer/index.tsx
@@ -1,28 +1,15 @@
-import cx from 'classnames'
 import * as React from 'react'
 import * as styles from './Footer.css'
 
-import NavLink from '@app/ui/atoms/NavLink'
+import BottomRow from './organisms/BottomRow'
 import MiddleRow from './organisms/MiddleRow'
 import TopRow from './organisms/TopRow'
 
-const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <TopRow className={styles.row} />
-      <MiddleRow className={styles.row} />
-      <div className={cx(styles.row, styles.bottom)}>
-        <div className={styles.oncohelp}>
-          <span className={styles.copyright}>© Просто спросить, 2018</span>
-          <NavLink className={styles.infoLink} type="nav">Пользовательское соглашение</NavLink>
-        </div>
-        <span className={styles.breadhead}>
-          <span className={styles.secondaryText}>Сайт сделан в </span>
-          <NavLink className={styles.infoLink} type="nav">Breadhead</NavLink>
-        </span>
-      </div>
-    </footer>
-  )
-}
+const Footer = () =>
+  <footer className={styles.footer}>
+    <TopRow className={styles.row} />
+    <MiddleRow className={styles.row} />
+    <BottomRow className={styles.row} />
+  </footer>
 
 export default Footer
