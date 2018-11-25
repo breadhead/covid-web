47cfd684e28e58393b25b54b8555ccfe0cdfa7f8
martyns0n <zogacc@gmail.com>
2018-11-25 13:04:10 +0300

    add switching of control for emerging form element

--- ui/molecules/EmergingFormElement/index.tsx
+++ ui/molecules/EmergingFormElement/index.tsx
@@ -2,37 +2,93 @@ import { Form as AntForm } from 'antd'
 import * as React from 'react'
 import { Field as FinalField } from 'react-final-form'
 
-import './Checkbox.css?CSSModulesDisable'
-
-const FormItem = AntForm.Item
+import Button from '@app/ui/molecules/Button'
+import RadioGroup from '@app/ui/molecules/RadioGroup'
+import Switch from '@app/ui/molecules/Switch'
+import { Recoverable } from 'repl'
+import * as styles from './EmergingFormElement.css'
 
 interface Props {
-  name: string
+  type: 'button' | 'switcher',
   className?: string
-  label?: string
   children?: React.ReactNode
 }
 
-const Checkbox = ({
-  name,
-  className,
-  label,
-  children,
-  ...rest
-}: Props) =>
-  <FinalField className={className} name={name}>
-    {({ meta }) => (
-      <FormItem
-        validateStatus={meta.submitError && 'error'}
-        help={meta.submitError}
-      >
-        <AntCheckbox
-          {...rest}
-        >
-          {children}
-        </AntCheckbox>
-      </FormItem>
-    )}
-  </FinalField>
+class EmergingFormElement extends React.Component {
 
-export default Checkbox
+  public testBoolRadioButtons: [
+    {
+      id: '1',
+      value: 'da',
+    },
+    {
+      id: '2',
+      value: 'niet',
+    },
+  ]
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      isTestFormElementVisible: false,
+      triggerPosition: 0,
+    }
+    this.toggleElement = this.toggleElement.bind(this)
+  }
+
+  public switchType(type) {
+    switch (type) {
+    case 'button':
+      return <Button
+        onClick={this.toggleElement}
+      >
+        Нажми меня
+      </Button>
+      break
+    case 'switcher':
+      return <Switch onChange={this.toggleElement} />
+      break
+    case 'radiogroup':
+      return <RadioGroup name="bool" type="bool" buttons={this.testBoolRadioButtons} />
+      break
+    default:
+      return null
+    }
+  }
+
+  public toggleElement(event) {
+    this.setState({
+      isTestFormElementVisible: !this.state.isTestFormElementVisible,
+      triggerPosition: event.target.offsetLeft + (event.target.offsetWidth / 2),
+    })
+    // console.log(event.target.offsetWidth)
+  }
+
+  public render() {
+    return (
+      <React.Fragment>
+        {
+          this.switchType(this.props.type)
+        }
+        {
+          <div
+            className={styles.EmergingFormElement}
+            hidden={this.state.isTestFormElementVisible}
+          >
+            <div
+              className={styles.TriggerPointer}
+              style={{
+                left: this.state.triggerPosition
+                  ? this.state.triggerPosition - 16
+                  : 0
+                + 'px',
+              }}/>
+            {this.props.children}
+          </div>
+        }
+      </React.Fragment>
+    )
+  }
+}
+
+export default EmergingFormElement
