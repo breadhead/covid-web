53220726207a01a8d7c58db425704469cd38b12d
martyns0n <zogacc@gmail.com>
2018-11-25 13:02:40 +0300

    add emerging form emelment

---
+++ ui/molecules/EmergingFormElement/index.tsx
@@ -0,0 +1,38 @@
+import { Form as AntForm } from 'antd'
+import * as React from 'react'
+import { Field as FinalField } from 'react-final-form'
+
+import './Checkbox.css?CSSModulesDisable'
+
+const FormItem = AntForm.Item
+
+interface Props {
+  name: string
+  className?: string
+  label?: string
+  children?: React.ReactNode
+}
+
+const Checkbox = ({
+  name,
+  className,
+  label,
+  children,
+  ...rest
+}: Props) =>
+  <FinalField className={className} name={name}>
+    {({ meta }) => (
+      <FormItem
+        validateStatus={meta.submitError && 'error'}
+        help={meta.submitError}
+      >
+        <AntCheckbox
+          {...rest}
+        >
+          {children}
+        </AntCheckbox>
+      </FormItem>
+    )}
+  </FinalField>
+
+export default Checkbox
