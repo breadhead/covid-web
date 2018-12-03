3bc16982f77453fc31983c4a9175e0a5fd5fe89e
martyns0n <zogacc@gmail.com>
2018-12-03 14:45:26 +0300

    add markup for sms-modal component

--- ui/organisms/ModalSmsConfirm/index.tsx
+++ ui/organisms/ModalSmsConfirm/index.tsx
@@ -51,11 +51,11 @@ export class PopupSmsConfirm extends Component {
         <Modal
           visible={this.state.visible}
           // onOk={this.handleOk}
-          // onCancel={this.handleCancel}
-          closable
-          footer={null}
+          onCancel={this.handleCancel}
           bodyStyle={{padding: '40px'}}
+          footer={null}
           width={400}
+          closable
         >
           <h2 className={styles.title}>СМС-код</h2>
           <p className={styles.description}>
@@ -65,7 +65,14 @@ export class PopupSmsConfirm extends Component {
               условиями обработки личных данных
             </a>
           </p>
-          <Input name="telephone" type="tel" label="Номер вашего телефона" required/>
+          <Input
+            onChange={this.sendSms}
+            name="telephone"
+            type="tel"
+            label="Номер вашего телефона"
+            defaultValue="+7"
+            required
+          />
           <div className={styles.blockGettingCode}>
             <Button onClick={this.sendSms}>Получить код</Button>
             <p className={styles.messageCodeSent} hidden={!this.state.isCodeSent}>
@@ -75,9 +82,13 @@ export class PopupSmsConfirm extends Component {
             </p>
           </div>
           <Input
+            name="sms code"
             type="number"
             label="Код из СМС"
             onChange={this.validateSmsCode}
+            addonAfter={
+              <Icon type="check" style={IconMessageCodeSentStyles}/>
+            }
             // TODO: define code max length
             maxLength="4"
             required
