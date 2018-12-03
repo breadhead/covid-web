541c89a3c183893726b238e78dec142fcc028bab
Igor Kamyshev <igor@kamyshev.me>
2018-12-03 17:52:52 +0300

    Add public config

--- lib/api/ApiClientFactory.ts
+++ lib/api/ApiClientFactory.ts
@@ -1,11 +1,14 @@
-import { BACK_URL } from '../config'
+import getConfig from 'next/config'
+
 import ApiClient from './ApiClient'
 import RealApiClient from './RealApiClient'
+
+const { publicRuntimeConfig } = getConfig()
 
 export default class ApiClientFactory {
   public static getApiClient(): ApiClient {
     if (!ApiClientFactory.apiClient) {
-      ApiClientFactory.apiClient = new RealApiClient(BACK_URL)
+      ApiClientFactory.apiClient = new RealApiClient(publicRuntimeConfig.backUrl)
     }
 
     return ApiClientFactory.apiClient
