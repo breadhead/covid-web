d00999f74ba328e2c925944bf5ffd9ac696eff50
iwanttobealight <iwanttobealight@yandex.ru>
2018-12-03 14:36:52 +0300

    [WIP] fix create-quota form

--- features/admin/createQuota/atoms/Select/index.tsx
+++ features/admin/createQuota/atoms/Select/index.tsx
@@ -1,16 +1,16 @@
-import { QuotaType } from '@app/models/Quota/Quota'
-import { default as UiSelect } from '@app/ui/molecules/Select'
+// import { QuotaType } from '@app/models/Quota/Quota'
+// import { default as UiSelect } from '@app/ui/molecules/Select'
 
-const Select = () => (
-  <UiSelect
-    name="category"
-    label="Категория типа квот"
-    defaultValue="Common"
-    options={Object
-      .values(QuotaType)
-      .map((value) => ({ id: value, value }))
-    }
-  />
-)
+// const Select = () => (
+//   <UiSelect
+//     name="category"
+//     label="Категория типа квот"
+//     defaultValue="Common"
+//     options={Object
+//       .values(QuotaType)
+//       .map((value) => ({ id: value, value }))
+//     }
+//   />
+// )
 
-export default Select
+// export default Select
