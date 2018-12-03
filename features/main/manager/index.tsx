fa71afec5215012eca5fe1cbe6a27457dd4353a4
iwanttobealight <iwanttobealight@yandex.ru>
2018-12-03 19:15:36 +0300

    add manager page

---
+++ features/main/manager/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../layout'
+
+const ManagerPage = () =>
+  <Layout
+    headerType="secondary"
+    footerType="secondary">
+    <div>Консультация</div>
+    <h1>Кратко о теме консультации</h1>
+  </Layout>
+
+export default ManagerPage
--- pages/main-client.tsx
+++ pages/main-client.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { default as Page } from '@app/features/main/client-page'
+import { default as Page } from '@app/features/main/client'
 
 class Client extends React.Component {
 
