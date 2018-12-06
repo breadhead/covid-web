c7e8fe946baac41d6cd93ca24ae5ad5015116205
Igor Kamyshev <igor@kamyshev.me>
2018-12-06 13:28:42 +0300

    fix(ui): add input for send sms

--- features/login/features/confirm/molecules/SendSms/index.tsx
+++ features/login/features/confirm/molecules/SendSms/index.tsx
@@ -3,19 +3,20 @@ import React from 'react'
 import { NON_BREAKING_SPACE } from '@app/lib/config'
 import Button from '@app/ui/atoms/Button'
 import Icon, { IconColor, IconType } from '@app/ui/atoms/Icon'
+import Input, { InputType } from '@app/ui/atoms/Input'
 
 import * as styles from './SendSms.css'
 
 const SendSms = () => (
   <>
-    {/* <Input
+    <Input
       name="number"
-      type="tel"
+      type={InputType.Number}
       label="Номер вашего телефона"
-      // TODO: fix default value attribute when the input component will be replaced for new one
       defaultValue="+7"
+      wrapperClassName={styles.inputWrapper}
       required
-    /> */}
+    />
     <div className={styles.blockGettingCode}>
       <Button>Получить код</Button>
       <p className={styles.messageCodeSent} hidden={false}>
---
+++ ui/atoms/Input/index.tsx
@@ -0,0 +1,33 @@
+import { Input as AntInput } from 'antd'
+import { InputProps } from 'antd/lib/input'
+import * as React from 'react'
+
+import './AntInput.css?CSSModulesDisable'
+import styles from './Input.css'
+
+export enum InputType {
+  Text = 'text',
+  Number = 'number',
+  Email = 'email',
+  Phone = 'tel',
+}
+
+type Props = {
+  name: string,
+  type?: InputType,
+  label?: string,
+  defaultValue?: string,
+  wrapperClassName?: string,
+} & InputProps
+
+const Input = ({ name, type, label, wrapperClassName, ...rest }: Props) =>
+  <div className={wrapperClassName}>
+    {label && <label className={styles.label} htmlFor={name}>{label}</label>}
+    <AntInput
+      id={name}
+      type={type}
+      {...rest}
+    />
+  </div>
+
+export default Input
