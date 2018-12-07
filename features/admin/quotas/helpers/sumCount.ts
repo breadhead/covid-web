9edf05d693d0402c9c3249714f94585c2346ed6b
iwanttobealight <iwanttobealight@yandex.ru>
2018-12-07 08:07:04 +0300

    fix(admin): fix quota sumCount func

--- features/admin/quotas/helpers/sumCount.ts
+++ features/admin/quotas/helpers/sumCount.ts
@@ -1,5 +1,7 @@
 import { Quota } from '@app/models/Quota/Quota'
 
-export const sumCount = (quotas: Quota[]) => quotas
-  .map((quota) => quota.count)
-  .reduce((prev, cur) => prev + cur)
+export const sumCount = (quotas: Quota[]) => quotas.length > 0
+  ? quotas
+    .map((quota) => quota.count)
+    .reduce((prev, cur) => prev + cur)
+  : 0
