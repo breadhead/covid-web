da779b4172867fa6c98a6a7da0513c2571862270
Igor Kamyshev <igor@kamyshev.me>
2018-12-10 11:21:33 +0300

    feat(admin): add initial fields on quota page

--- features/admin/features/editQuota/container.tsx
+++ features/admin/features/editQuota/container.tsx
@@ -7,9 +7,12 @@ import {
   Props as FormProps,
   QuotaFields,
 } from '@app/features/admin/features/quotaForm'
+import { fetchQuota, getQuota } from '@app/features/admin/quota'
 import { push as pushNotification } from '@app/features/admin/toast'
 import { QuotaEditRequest } from '@app/lib/api/request/Quota'
+import { AppContext } from '@app/lib/server-types'
 import { State } from '@app/lib/store'
+import { Quota } from '@app/models/Quota/Quota'
 
 import { editQuota } from './actions'
 import { getEditedQuotaError, getEditedQuotaId } from './selectors'
@@ -17,20 +20,47 @@ import { getEditedQuotaError, getEditedQuotaId } from './selectors'
 interface Props {
   error: boolean | string
   editQuota: (request: QuotaEditRequest) => Promise<any>
+  quota?: Quota
   editedQuotaId?: string
+}
+
+interface Query {
+  id: string
 }
 
 const Container = (WrappedComponent: React.ComponentType<FormProps>) => {
   return class extends React.Component<Props> {
+    public static async getInitialProps(context: AppContext<Query>) {
+      await context.reduxStore.dispatch(fetchQuota(context.query.id) as any)
+      return {}
+    }
+
     public render() {
+      const { quota } = this.props
+
+      const initalFields = quota ? this.quotaToFields(quota) : undefined
+
       return (
         <WrappedComponent
           handleQuota={this.onFormSubmit}
           afterSuccess={this.afterSuccess}
+          initial={initalFields}
           {...this.props}
         />
       )
     }
+
+    private quotaToFields = (quota: Quota): QuotaFields => ({
+      name: quota.name,
+      category: quota.type.toString(),
+      companyName: quota.company.name,
+      comment: quota.comment,
+      count: quota.count.toString(),
+      publicCompany: quota.publicCompany.toString(),
+      logo: quota.company.logo,
+      companyLink: quota.company.site,
+      logotypeComment: '', // TODO: get real logotypeComment
+    })
 
     private onFormSubmit = (quotaFields: QuotaFields) => {
       // TODO: create postQuotaFields from quotaFields
@@ -51,6 +81,7 @@ const Container = (WrappedComponent: React.ComponentType<FormProps>) => {
 const mapState = (state: State) => ({
   error: getEditedQuotaError(state),
   editedQuotaId: getEditedQuotaId(state),
+  quota: getQuota(state),
 })
 
 const mapDipatch = (dispatch: Dispatch<AnyAction>) => ({
