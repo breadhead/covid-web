47b639e65655b420a4324e31184eefd80c62aa6e
uncleseneca <uncleseneca@gmail.com>
2018-12-19 14:22:44 +0300

    fix(ui): fix icons

--- ui/atoms/Logo/index.tsx
+++ ui/atoms/Logo/index.tsx
@@ -1,17 +1,12 @@
 import IconCustom from '@app/ui/atoms/IconCustom'
-import cx from 'classnames'
 import * as React from 'react'
-import * as styles from './Logo.css'
 
 interface Props {
   className?: string
 }
 
 const Logo = ({ className }: Props) => (
-  <>
-    <IconCustom className={cx(styles.logoFull, className)} name="logo_full" />
-    <IconCustom className={cx(styles.logoShort, className)} name="logo_short" />
-  </>
+  <IconCustom className={className} name="logo_short" />
 )
 
 export default Logo
