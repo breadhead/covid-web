8f96330f490648fc699d1e123c581863052b23d6
uncleseneca <uncleseneca@gmail.com>
2018-12-19 18:32:43 +0300

    fix(ui): fix footer layout

--- ui/atoms/Logo/index.tsx
+++ ui/atoms/Logo/index.tsx
@@ -1,12 +1,14 @@
 import IconCustom from '@app/ui/atoms/IconCustom'
+import cx from 'classnames'
 import * as React from 'react'
+import styles from './Logo.css'
 
 interface Props {
   className?: string
 }
 
 const Logo = ({ className }: Props) => (
-  <IconCustom className={className} name="logo_short" />
+  <IconCustom className={cx(styles.logo, className)} name="logo_short" />
 )
 
 export default Logo
