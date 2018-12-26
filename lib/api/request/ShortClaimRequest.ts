c44450c3fdd80cdb317e1f7ff04f7cc6a6210c65
Igor Kamyshev <igor@kamyshev.me>
2018-12-26 13:53:44 +0300

    fix(global): use utility type

--- lib/api/request/ShortClaimRequest.ts
+++ lib/api/request/ShortClaimRequest.ts
@@ -1,7 +1,6 @@
-import { ShortClaim } from '@app/models/Claim/ShortClaim'
+import { Omit } from 'utility-types'
 
-// Omit taken from https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+import { ShortClaim } from '@app/models/Claim/ShortClaim'
 
 type ShortClaimRequest = Omit<ShortClaim, 'id' | 'quotaAllocated'>
 
