602eda0385a43fb7cb6d596417b736a9bfb68840
uncleseneca <uncleseneca@gmail.com>
2018-12-27 23:22:05 +0300

    fix(client): fix selectYear

--- ui/organisms/CustomElements/SelectYears/config.ts
+++ ui/organisms/CustomElements/SelectYears/config.ts
@@ -1,42 +1,42 @@
 export const OPTIONS_YEARS = [
   {
-    key: '1',
+    key: '2018',
     label: '2018',
   },
   {
-    key: '2',
+    key: '2017',
     label: '2017',
   },
   {
-    key: '3',
+    key: '2016',
     label: '2016',
   },
   {
-    key: '4',
+    key: '2015',
     label: '2015',
   },
   {
-    key: '5',
+    key: '2014',
     label: '2014',
   },
   {
-    key: '6',
+    key: '2013',
     label: '2013',
   },
   {
-    key: '7',
+    key: '2012',
     label: '2012',
   },
   {
-    key: '8',
+    key: '2011',
     label: '2011',
   },
   {
-    key: '9',
+    key: '2010',
     label: '2010',
   },
   {
-    key: '10',
+    key: '2009',
     label: '2009',
   },
 ]
