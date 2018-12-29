a8687b4ef808ebeb029665501a25af1969284da9
iwanttobealight <iwanttobealight@yandex.ru>
2018-12-29 12:55:01 +0300

    feat(client): add scrollToBottom func to chat

--- features/client/features/chat/organisms/ChatWrapper/index.tsx
+++ features/client/features/chat/organisms/ChatWrapper/index.tsx
@@ -12,15 +12,36 @@ interface Props {
   messages: ChatMessage[]
 }
 
-const ChatWrapper = ({ messages }: Props) =>
-  messages.length === 0 ? (
-    <EmptyWindow text={EmptyWindowText} />
-  ) : (
-    <div className={styles.chatWrapper}>
-      {messages.map(message => (
-        <Message key={message.id} message={message} />
-      ))}
-    </div>
-  )
+class ChatWrapper extends React.Component<Props> {
+  private myRef = React.createRef<HTMLDivElement>()
+
+  public scrollToBottom = () => {
+    if (this.myRef.current) {
+      this.myRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  public componentDidMount() {
+    this.scrollToBottom()
+  }
+
+  public componentDidUpdate() {
+    this.scrollToBottom()
+  }
+
+  public render() {
+    const { messages } = this.props
+
+    return messages.length === 0 ? (
+      <EmptyWindow text={EmptyWindowText} />
+    ) : (
+      <div className={styles.chatWrapper} ref={this.myRef}>
+        {messages.map(message => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+    )
+  }
+}
 
 export default ChatWrapper
