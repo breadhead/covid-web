3a465f736583e2d60b2f57b0b621aa2dafae3f2b
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-09 14:29:36 +0300

    feat(client): add sort for claims on client/claims page

---
+++ features/client/features/claims/organisms/Claims/helpers.ts
@@ -0,0 +1,12 @@
+import { ListedClaim } from '@app/models/Claim/ListedClaim'
+
+export const compareNumeric = (a: ListedClaim, b: ListedClaim) => {
+  if (new Date(a.createdAt).getTime() > new Date(b.createdAt).getTime()) {
+    return -1
+  }
+  if (new Date(a.createdAt).getTime() < new Date(b.createdAt).getTime()) {
+    return 1
+  } else {
+    return 0
+  }
+}
--- features/client/features/claims/organisms/Claims/index.tsx
+++ features/client/features/claims/organisms/Claims/index.tsx
@@ -3,13 +3,15 @@ import { ListedClaim } from '@app/models/Claim/ListedClaim'
 import ClaimCard from '../Claim'
 import styles from './Claims.css'
 
+import { compareNumeric } from './helpers'
+
 interface Props {
   claims: ListedClaim[]
 }
 
 const Claims = ({ claims }: Props) => (
   <section className={styles.list}>
-    {claims.map(claim => (
+    {claims.sort(compareNumeric).map((claim: ListedClaim) => (
       <ClaimCard key={claim.id} claim={claim} />
     ))}
   </section>
