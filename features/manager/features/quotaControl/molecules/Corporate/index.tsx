61e7b11eca493d2769f081e62c5ae2cb7ec9e0b8
Igor Kamyshev <igor@kamyshev.me>
2019-01-11 15:39:35 +0300

    feat(admin): finalize layout for consultation in manager

--- features/manager/features/quotaControl/molecules/Corporate/index.tsx
+++ features/manager/features/quotaControl/molecules/Corporate/index.tsx
@@ -1,5 +1,18 @@
+import Select from '@app/ui/atoms/Select'
+
 import * as styles from './Corporate.css'
 
-const Corporate = () => <div className={styles.corporate}>fdsfd</div>
+const options = [
+  {
+    key: '1',
+    label: 'Проверка корпоративности',
+  },
+]
+
+const Corporate = () => (
+  <div className={styles.corporate}>
+    <Select name="fdf" options={options} defaultValue={'1'} />
+  </div>
+)
 
 export default Corporate
