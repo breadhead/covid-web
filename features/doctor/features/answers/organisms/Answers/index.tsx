aee456ca0a537c6340d5add66113d3a3a932d6eb
Igor Kamyshev <igor@kamyshev.me>
2019-01-14 16:24:51 +0300

    feat(doctor): add styling to answer form

--- features/doctor/features/answers/organisms/Answers/index.tsx
+++ features/doctor/features/answers/organisms/Answers/index.tsx
@@ -5,29 +5,34 @@ import { Form, TextArea } from '@app/features/common/form'
 import { AnswerClaim } from '@app/models/Claim/AnswerClaim'
 import Button, { ButtonKind, ButtonType } from '@app/ui/atoms/Button'
 
+import * as styles from './Answers.css'
+
 interface Props {
   claim: AnswerClaim
 }
 
 const Answers = ({ claim }: Props) => (
-  <>
-    <Form onSubmit={console.log as any}>
-      <ExpertAnswers
-        claim={claim}
-        title="Вопросы эксперту"
-        renderCustomAnswer={(theme, { question }) => (
-          <TextArea name={`answers[${theme}: ${question}]`} />
-        )}
-      />
+  <Form onSubmit={console.log as any}>
+    <ExpertAnswers
+      claim={claim}
+      title="Вопросы эксперту"
+      renderCustomAnswer={(theme, { question }) => (
+        <TextArea
+          className={styles.textarea}
+          name={`answers[${theme}: ${question}]`}
+        />
+      )}
+    />
+    <div className={styles.controls}>
+      <Button type={ButtonType.Submit}>Отправить ответ</Button>
       <Button
         kind={ButtonKind.Secondary}
         onClick={() => Router.push(`/doctor/consultation/${claim.id}`)}
       >
         Отменить
       </Button>
-      <Button type={ButtonType.Submit}>Отправить ответ</Button>
-    </Form>
-  </>
+    </div>
+  </Form>
 )
 
 export default Answers
