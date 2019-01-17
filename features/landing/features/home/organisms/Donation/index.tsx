d939300030db858114029940664cfd2f5eabd24e
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-17 18:46:15 +0300

    feat(global): fix footer

--- features/landing/features/home/organisms/Donation/index.tsx
+++ features/landing/features/home/organisms/Donation/index.tsx
@@ -15,14 +15,13 @@ const Donation = () => (
         <article className={styles.quote}>
           <p className={styles.message}>
             Все собранные средства идут на{NON_BREAKING_SPACE}оплату экспертов,
-            задействованных в{NON_BREAKING_SPACE} консультациях и
-            {NON_BREAKING_SPACE}
-            {NON_BREAKING_SPACE} на{NON_BREAKING_SPACE} поддержание
+            задействованных в{NON_BREAKING_SPACE}консультациях и
+            {NON_BREAKING_SPACE}на{NON_BREAKING_SPACE}поддержание
             работоспособности сервиса. Поддерживая системные проекты:
             образование талантливых врачей, просвещение населения РФ, внедрение
-            технологий скрининга рака, можно спасти сотни и{NON_BREAKING_SPACE}{' '}
-            тысячи неизвестных вам людей. И{NON_BREAKING_SPACE} Бог знает, кто
-            будут эти люди – возможно они будут дороги и{NON_BREAKING_SPACE}{' '}
+            технологий скрининга рака, можно спасти сотни и{NON_BREAKING_SPACE}
+            тысячи неизвестных вам людей. И{NON_BREAKING_SPACE}Бог знает, кто
+            будут эти люди – возможно они будут дороги и{NON_BREAKING_SPACE}
             лично вам.
           </p>
           <div className={styles.author}>
