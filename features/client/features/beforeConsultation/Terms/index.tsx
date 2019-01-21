1115daab2d666be8b311773517c9e9e91269802f
Igor Kamyshev <igor@kamyshev.me>
2019-01-21 14:43:51 +0300

    feat(client): add before consultation page before consultation

--- features/client/features/beforeConsultation/Terms/index.tsx
+++ features/client/features/beforeConsultation/Terms/index.tsx
@@ -1,18 +1,62 @@
+import Router from 'next/router'
 import * as React from 'react'
+
+import {
+  ButtonWithTooltip,
+  Checkbox,
+  Form,
+  ButtonType,
+} from '@app/features/common/form'
+
 import styles from './Terms.css'
 
-import Button from '@app/ui/atoms/Button'
-import Checkbox from '@app/ui/atoms/Checkbox'
+interface Fields {
+  consultationTerms: boolean
+  personalDataConditions: boolean
+}
 
-const Terms = () => (
-  <>
-    <Checkbox className={styles.checkbox} name="consultation-terms">
-      Я согласен с <a href="#">условиями получения консультации</a>
-    </Checkbox>
-    <Checkbox className={styles.checkbox} name="personal-data-conditions">
-      Я согласен с <a href="#">условиями обработки персональных данных</a>
-    </Checkbox>
-    <Button className={styles.button}>Начать консультацию</Button>
-  </>
-)
+interface State {
+  failed: boolean
+}
+
+class Terms extends React.Component<{}, State> {
+  public state = {
+    failed: false,
+  } as State
+
+  public render() {
+    return (
+      <Form onSubmit={this.onSubmit as any}>
+        <Checkbox name="consultationTerms" className={styles.checkbox}>
+          Я согласен с <a href="#">условиями получения консультации</a>
+        </Checkbox>
+        <Checkbox name="personalDataConditions" className={styles.checkbox}>
+          Я согласен с <a href="#">условиями обработки персональных данных</a>
+        </Checkbox>
+        <ButtonWithTooltip
+          error={this.getError()}
+          type={ButtonType.Submit}
+          className={styles.button}
+        >
+          Начать консультацию
+        </ButtonWithTooltip>
+      </Form>
+    )
+  }
+
+  private onSubmit = ({
+    consultationTerms,
+    personalDataConditions,
+  }: Fields) => {
+    if (!consultationTerms || !personalDataConditions) {
+      return this.setState({ failed: true })
+    }
+
+    this.setState({ failed: false })
+    return Router.push('/client/new-claim')
+  }
+
+  private getError = () =>
+    this.state.failed ? 'Пожалуйста, примите условия' : undefined
+}
 export default Terms
