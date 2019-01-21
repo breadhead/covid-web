2cdfdcd25f0fe489a22e7d671df858ee23b84213
uncleseneca <uncleseneca@gmail.com>
2019-01-21 16:27:14 +0300

    fix(client): add client redirect to before-consultation page on signUp

--- features/login/features/redirect/index.ts
+++ features/login/features/redirect/index.ts
@@ -10,15 +10,17 @@ export default (roles: string[], condition?: Condition) => {
   if (roles.includes(Role.Admin)) {
     Router.push('/admin')
   } else if (roles.includes(Role.Client)) {
-    debugger
-    if (condition === Condition.signUp) {
-      Router.push('/client/before-consultation')
-    } else {
-      Router.push('/client')
-    }
+    redirectUser(condition)
   } else if (roles.includes(Role.CaseManager)) {
     Router.push('/manager')
   } else if (roles.includes(Role.Doctor)) {
     Router.push('/doctor')
+  }
+}
+const redirectUser = (condition?: Condition) => {
+  if (condition === Condition.signUp) {
+    Router.push('/client/before-consultation')
+  } else {
+    Router.push('/client')
   }
 }
