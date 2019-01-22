455fbd6709a347bcd612ab4f1a7ab158ea36b005
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-22 19:59:36 +0300

    feat(ui): add loader to questions submit button

--- features/client/features/editClaim/features/Questions/page/index.tsx
+++ features/client/features/editClaim/features/Questions/page/index.tsx
@@ -9,14 +9,16 @@ import { ClaimData } from '../types'
 export interface Props {
   onFormSubmit: (fields: QuestionsClaim) => Promise<QuestionsClaim>
   error: false | string
+  loading: boolean
   claimData: ClaimData
 }
 
-const ClaimPage = ({ error, onFormSubmit, claimData }: Props) => (
+const ClaimPage = ({ error, loading, onFormSubmit, claimData }: Props) => (
   <Layout step={3} title="Отметьте вопросы, которые хотите задать эксперту">
     <ClaimForm
       claimData={claimData}
       error={error}
+      loading={loading}
       onFormSubmit={onFormSubmit}
     />
   </Layout>
