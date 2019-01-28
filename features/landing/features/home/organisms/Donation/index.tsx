1f78f29dc605212d99b306cef29acd15d3243a61
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-28 19:16:29 +0300

    feat(common): add donation widget to landing page

--- features/landing/features/home/organisms/Donation/index.tsx
+++ features/landing/features/home/organisms/Donation/index.tsx
@@ -2,44 +2,18 @@ import * as React from 'react'
 
 import * as styles from './Donation.css'
 
-import { NON_BREAKING_SPACE } from '@app/lib/config'
+import DonationWidget from '@app/features/main/donation'
+import Info from '../Info'
 
 const Donation = () => (
   <>
     <div id="donation" className={styles.donationWrapper}>
       <h2 className={styles.title}>Помочь проекту</h2>
       <div className={styles.content}>
-        <div className={styles.iframe}>
-          <iframe src="https://nenaprasno.ru" width="672" height="5800" />
+        <div className={styles.widget}>
+          <DonationWidget />
         </div>
-        <article className={styles.quote}>
-          <p className={styles.message}>
-            Все собранные средства идут на{NON_BREAKING_SPACE}оплату экспертов,
-            задействованных в{NON_BREAKING_SPACE}консультациях и
-            {NON_BREAKING_SPACE}на{NON_BREAKING_SPACE}поддержание
-            работоспособности сервиса. Поддерживая системные проекты:
-            образование талантливых врачей, просвещение населения РФ, внедрение
-            технологий скрининга рака, можно спасти сотни и{NON_BREAKING_SPACE}
-            тысячи неизвестных вам людей. И{NON_BREAKING_SPACE}Бог знает, кто
-            будут эти люди – возможно они будут дороги и{NON_BREAKING_SPACE}
-            лично вам.
-          </p>
-          <div className={styles.author}>
-            <img
-              className={styles.authorPhoto}
-              src="/static/images/fomin_avatar.jpg"
-              alt="Илья Фоминцев"
-            />
-            <div className={styles.textWrapper}>
-              <p className={styles.authorName}>Илья Фоминцев</p>
-              <p className={styles.authorPosition}>
-                Исполнительный директор
-                <br />
-                «Фонда профилактики рака»
-              </p>
-            </div>
-          </div>
-        </article>
+        <Info />
       </div>
     </div>
   </>
