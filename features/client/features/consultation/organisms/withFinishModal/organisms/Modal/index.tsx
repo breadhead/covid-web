83c0937b196d2cf7dbf155d0c0b644c6fb1040b7
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-28 19:29:03 +0300

    feat(client): [WIP] add wiget to consultation popup

--- features/client/features/consultation/organisms/withFinishModal/organisms/Modal/index.tsx
+++ features/client/features/consultation/organisms/withFinishModal/organisms/Modal/index.tsx
@@ -2,8 +2,10 @@ import * as React from 'react'
 
 import * as styles from './Modal.css'
 
+import DonationWidget from '@app/features/main/donation'
 import { NON_BREAKING_SPACE } from '@app/lib/config'
 import ClaimStatus from '@app/models/Claim/ClaimStatus'
+import Info from '../Info'
 
 interface Props {
   closeClaim: (id: string) => Promise<void>
@@ -31,28 +33,10 @@ class Modal extends React.Component<Props> {
           Вы поможете другим людям, оформив пожертвование на{NON_BREAKING_SPACE}
           этой странице
         </h1>
-        <article className={styles.quotes}>
-          <p className={styles.message}>
-            Ваше пожертвование поможет другим получить консультацию как можно
-            скорее. Подписка на{NON_BREAKING_SPACE}регулярный платеж в
-            {NON_BREAKING_SPACE}пользу Фонда даже с{NON_BREAKING_SPACE}маленькой
-            суммой позволит нам развивать сервис и{NON_BREAKING_SPACE}
-            поддерживать его работу.
-          </p>
-          <div className={styles.author}>
-            <img
-              className={styles.authorPhoto}
-              src="/static/images/fomin_avatar.jpg"
-              alt="Илья Фоминцев"
-            />
-            <div className={styles.textWrapper}>
-              <p className={styles.authorName}>Илья Фоминцев</p>
-              <p className={styles.authorPosition}>
-                Исполнительный директор «Фонда профилактики рака»
-              </p>
-            </div>
-          </div>
-        </article>
+        <Info />
+        <div className={styles.widget}>
+          <DonationWidget />
+        </div>
       </article>
     )
   }
