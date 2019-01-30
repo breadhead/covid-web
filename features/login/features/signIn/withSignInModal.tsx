6f9416950c089b80292d03b281a900bc13483dc9
uncleseneca <uncleseneca@gmail.com>
2019-01-30 13:43:48 +0300

    refactor: refactor modal buttons to openModal button

--- features/login/features/signIn/withSignInModal.tsx
+++ features/login/features/signIn/withSignInModal.tsx
@@ -2,12 +2,12 @@ import { withModal, WithModalProps } from '@app/features/common/modal'
 import { SIGN_IN_MODAL } from '@app/features/login'
 
 export interface WithLoginModal {
-  openLogin: () => void
+  openSignIn: () => void
 }
 
 const Container = (Component: React.ComponentType<WithLoginModal>) =>
   withModal(({ modal, ...props }: WithModalProps) => (
-    <Component {...props} openLogin={() => modal.open(SIGN_IN_MODAL)} />
+    <Component {...props} openSignIn={() => modal.open(SIGN_IN_MODAL)} />
   ))
 
 export default Container
