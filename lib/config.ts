78a85aaa90ee47533edc59cda8dc909e341eb9f0
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-31 14:15:55 +0300

    feat(landing): add new expert

--- lib/config.ts
+++ lib/config.ts
@@ -4,6 +4,8 @@ export const NON_BREAKING_SPACE = '\u00A0'
 
 export const NON_BREAKING_HYPHEN = '\u2011'
 
+export const ASTERISK = '\u002A'
+
 export const MOBILE_WIDTH = 767
 
 export const CHAT_DEFAULT_OPEN_WIDTH = 959
