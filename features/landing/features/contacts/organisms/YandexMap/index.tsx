cb42ccb372fde5eb8e569ed2638a5bab9baee052
iwanttobealight <iwanttobealight@yandex.ru>
2019-01-31 14:46:11 +0300

    fix(landing): add background to yandex map

--- features/landing/features/contacts/organisms/YandexMap/index.tsx
+++ features/landing/features/contacts/organisms/YandexMap/index.tsx
@@ -13,43 +13,41 @@ const LON = 30.28458858355316
 class YandexMap extends React.Component {
   public render() {
     return (
-      <>
-        <div className={styles.map}>
-          <YMaps>
-            <Map
-              className={styles.mapContainer}
-              defaultState={{ center: [LAT, LON], zoom: 16 }}
-              defaultOptions={{ behaviors: [{ scrollZoom: false }] }}
-              instanceRef={(ref: any) => {
-                // tslint:disable-next-line:no-unused-expression
-                ref && ref.behaviors.disable('scrollZoom')
-              }}
-            >
-              <Placemark geometry={[LAT, LON]} />
-              <article className={styles.infoBlock}>
-                <p className={styles.text}>
-                  <ExternalLink className={styles.link} href="tel:+78124685797">
-                    +7 812 468-57-97
-                  </ExternalLink>
-                </p>
-                <p className={styles.text}>
-                  <ExternalLink
-                    className={styles.link}
-                    href="mailto:fond@nenaprasno.ru"
-                  >
-                    fond@nenaprasno.ru
-                  </ExternalLink>
-                </p>
-                <p className={styles.text}>
-                  Санкт-Петербург, 2-я{NON_BREAKING_SPACE}линия Васильевского
-                  {NON_BREAKING_SPACE}
-                  острова, д.{NON_BREAKING_SPACE}37, офис{NON_BREAKING_SPACE}310
-                </p>
-              </article>
-            </Map>
-          </YMaps>
-        </div>
-      </>
+      <div className={styles.map}>
+        <YMaps>
+          <Map
+            className={styles.mapContainer}
+            defaultState={{ center: [LAT, LON], zoom: 16 }}
+            defaultOptions={{ behaviors: [{ scrollZoom: false }] }}
+            instanceRef={(ref: any) => {
+              // tslint:disable-next-line:no-unused-expression
+              ref && ref.behaviors.disable('scrollZoom')
+            }}
+          >
+            <Placemark geometry={[LAT, LON]} />
+            <article className={styles.infoBlock}>
+              <p className={styles.text}>
+                <ExternalLink className={styles.link} href="tel:+78124685797">
+                  +7 812 468-57-97
+                </ExternalLink>
+              </p>
+              <p className={styles.text}>
+                <ExternalLink
+                  className={styles.link}
+                  href="mailto:fond@nenaprasno.ru"
+                >
+                  fond@nenaprasno.ru
+                </ExternalLink>
+              </p>
+              <p className={styles.text}>
+                Санкт-Петербург, 2-я{NON_BREAKING_SPACE}линия Васильевского
+                {NON_BREAKING_SPACE}
+                острова, д.{NON_BREAKING_SPACE}37, офис{NON_BREAKING_SPACE}310
+              </p>
+            </article>
+          </Map>
+        </YMaps>
+      </div>
     )
   }
 }
