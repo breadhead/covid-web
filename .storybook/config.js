9d43f264e141a2e3d30fa05f9d3f031bdc8c3232
Igor Kamyshev <igor@kamyshev.me>
2019-02-06 14:51:45 +0200

    fix: add miss deps for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
 import { configure } from '@storybook/react'
 
-import '../src/ui/config.css'
-
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.tsx$/)
+
+import '../src/ui/config.css'
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
