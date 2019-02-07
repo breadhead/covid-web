4055a9c1fbd1c18e39b882660974f70d0deeb4d8
uncleseneca <uncleseneca@gmail.com>
2019-02-07 17:48:01 +0200

    fix: fix footer in situation

--- features/client/features/claim/features/editClaim/features/Situation/organisms/Footer/index.tsx
+++ features/client/features/claim/features/editClaim/features/Situation/organisms/Footer/index.tsx
@@ -8,28 +8,38 @@ import {
   ButtonWithTooltip,
 } from '@app/features/common/form'
 import { StylesType } from '@app/lib/config'
+import IconCustom from '@app/ui/atoms/IconCustom'
+import NavLink from '@app/ui/atoms/NavLink'
 
 interface Props {
   styles: StylesType
   error: false | string
-  loading: boolean
+  loading?: boolean
+  id: string
 }
 
 const ERROR_MESSAGE = 'Произошла ошибка, попробуйте еще раз'
 
-const Footer = ({ styles, error, loading }: Props) => {
+const Footer = ({ styles, error, loading, id }: Props) => {
   const errorMessage = error ? ERROR_MESSAGE : undefined
   return (
     <footer className={cx(styles.article, styles.footer)}>
       <ButtonWithTooltip
         error={errorMessage}
+        loading={loading}
         size={ButtonSize.Large}
         type={ButtonType.Submit}
         className={styles.button}
-        loading={loading}
       >
         Продолжить
       </ButtonWithTooltip>
+      <div className={styles.footerBack}>
+        <IconCustom
+          className={styles.iconBack}
+          name="24x24_arrow-small_right"
+        />
+        <NavLink href={`/client/new-claim/${id}`}>Вернуться назад</NavLink>
+      </div>
     </footer>
   )
 }
