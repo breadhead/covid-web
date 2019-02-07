09a5e4a6fac1011ee52f868ac02e94fafd973649
Igor Kamyshev <igor@kamyshev.me>
2019-02-07 07:48:56 +0200

    feat: tmp remove new icon

--- src/ui/atoms/icon/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import My from './resources/fund.svg?sprite'
-
-import { IconType } from './IconType'
-
-interface Props {
-  type?: IconType
-}
-
-export const Icon = ({ type }: Props) => <My className={type} />
