e14a483f8b53c614e3482987938f96815dce8255
Igor Kamyshev <igor@kamyshev.me>
2019-02-08 16:08:45 +0200

    fix: remove prettier ignore

--- src/ui/atoms/extarnal-link/__dev__/ExternalLink.stories.tsx
+++ src/ui/atoms/extarnal-link/__dev__/ExternalLink.stories.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 import { ExternalLink } from '../ExternalLink'
 
-// prettier-ignore
-storiesOf('atoms/ExternalLink', module)
-  .add('simple link', () => <ExternalLink href="google.com">Google</ExternalLink>)
+storiesOf('atoms/ExternalLink', module).add('simple link', () => (
+  <ExternalLink href="google.com">Google</ExternalLink>
+))
