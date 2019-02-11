08325ea785c30ace087cfb7ffac199d85970a68f
uncleseneca <uncleseneca@gmail.com>
2019-02-11 10:58:08 +0200

    fix: fix tests

--- lib/helpers/validateDates/__tests__/getDateInSeconds.spec.ts
+++ lib/helpers/validateDates/__tests__/getDateInSeconds.spec.ts
@@ -1,7 +1,10 @@
+import dayjs from 'dayjs'
 import { getDateInSeconds } from '../getDateInSeconds'
 
 describe('getDateInSeconds works', () => {
   test('should return a correct date', () => {
-    expect(getDateInSeconds({ year: 2018, month: 1 })).toBe(1514757600000)
+    expect(getDateInSeconds({ year: 2018, month: 1 })).toBe(
+      dayjs('2018-1-1').valueOf(),
+    )
   })
 })
