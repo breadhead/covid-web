3de513916ecd8c515be48091489f1c5f260ead2a
iwanttobealight <iwanttobealight@yandex.ru>
2019-02-13 15:33:37 +0300

    feat(manager): add back link to client claims page

--- features/manager/features/clientClaims/page/index.tsx
+++ features/manager/features/clientClaims/page/index.tsx
@@ -6,14 +6,25 @@ import { Layout } from '@app/features/manager'
 
 import Claims from '@app/features/client/features/claims/organisms/Claims'
 import { ListedClaim } from '@app/models/Claim/ListedClaim'
+import IconCustom from '@app/ui/atoms/IconCustom'
+import NavLink from '@app/ui/atoms/NavLink'
 
 export interface Props {
   clientClaims: ListedClaim[]
   authorLogin: string
+  currentClaimId?: string
 }
 
-const ClientClaims = ({ clientClaims, authorLogin }: Props) => (
+const ClientClaims = ({ clientClaims, authorLogin, currentClaimId }: Props) => (
   <Layout className={styles.clientClaims}>
+    <div className={styles.linkWrapper}>
+      <IconCustom name="24x24_arrow-small_right" className={styles.icon} />
+      {!!currentClaimId && (
+        <NavLink href={`/manager/consultation/${currentClaimId}`}>
+          Вернуться назад
+        </NavLink>
+      )}
+    </div>
     <header className={styles.header}>
       <h1 className={styles.title}>{clientClaims.length} консультации</h1>
       <p className={styles.authorLogin}>{authorLogin}</p>
