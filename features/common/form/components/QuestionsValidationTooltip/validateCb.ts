d7a42f9abd5085c41acb1020b0803c222070810a
iwanttobealight <iwanttobealight@yandex.ru>
2019-02-15 13:53:17 +0300

    fix(client): fix validateCb function

--- features/common/form/components/QuestionsValidationTooltip/validateCb.ts
+++ features/common/form/components/QuestionsValidationTooltip/validateCb.ts
@@ -6,9 +6,11 @@ const MAX_QUESTIONS_COUNT = 5
 const ERROR_TEXT = 'Пожалуйста, выберите не более 5 вопросов'
 
 export const validateCb = (_name: any, values: Values) => {
-  const currentValues = Object.values(values.defaultQuestions)
-  const trulyValues = currentValues.filter(val => !!val)
-  if (trulyValues.length > MAX_QUESTIONS_COUNT) {
-    throw new Error(ERROR_TEXT)
+  if (Object.keys(values).length > 0) {
+    const currentValues = Object.values(values.defaultQuestions)
+    const trulyValues = currentValues.filter(val => !!val)
+    if (trulyValues.length > MAX_QUESTIONS_COUNT) {
+      throw new Error(ERROR_TEXT)
+    }
   }
 }
