99a7154b700e957dcce12eda6c7464d82e8444c5
iwanttobealight <iwanttobealight@yandex.ru>
2019-02-19 19:05:14 +0300

    feat(claim card): fix styles on closedWithoutAnswer status

--- features/client/features/claims/molecules/ClaimStatus/helpers/isActive.ts
+++ features/client/features/claims/molecules/ClaimStatus/helpers/isActive.ts
@@ -1,5 +1,5 @@
 import ClaimStatus from '@app/models/Claim/ClaimStatus'
 
-const inavtiveStatuses = [ClaimStatus.Closed]
+const inavtiveStatuses = [ClaimStatus.Closed, ClaimStatus.ClosedWithoutAnswer]
 
 export default (status: ClaimStatus) => !inavtiveStatuses.includes(status)
