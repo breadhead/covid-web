7db31fbf7adc08817b7f723e8dbdf4f78f7ff861
iwanttobealight <iwanttobealight@yandex.ru>
2019-02-25 11:44:25 +0500

    feat(manager): add refuse-modal styles

--- features/manager/features/closeClaim/organisms/RefuseModal/index.tsx
+++ features/manager/features/closeClaim/organisms/RefuseModal/index.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react'
+import * as styles from './RefuseModal.css'
 
 import { isModal, withModal } from '@app/features/common/modal'
+import Button, { ButtonKind, ButtonSize } from '@app/ui/atoms/Button'
 import { compose } from 'recompose'
 import { MODAL_KEY } from './const'
 
-const RefuseModal = () => <article>memem memem memmeme </article>
+const RefuseModal = () => (
+  <article className={styles.modal}>
+    <h1 className={styles.title}>
+      Вы действительно хотите сменить статус на «Отказано»?
+    </h1>
+    <div className={styles.buttons}>
+      <Button
+        size={ButtonSize.Large}
+        kind={ButtonKind.Secondary}
+        className={styles.cancel}
+      >
+        Отмена
+      </Button>
+      <Button size={ButtonSize.Large}>Да, изменить статус</Button>
+    </div>
+  </article>
+)
 
 export default compose(
   isModal(MODAL_KEY),
