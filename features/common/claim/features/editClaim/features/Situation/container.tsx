32472231bf135c60d2357ecead235211aaee6c8c
Igor Kamyshev <igor@kamyshev.me>
2019-03-04 11:47:26 +0200

    fix(uploader): filter empty files

--- features/common/claim/features/editClaim/features/Situation/container.tsx
+++ features/common/claim/features/editClaim/features/Situation/container.tsx
@@ -132,10 +132,12 @@ const Container = (WrappedComponent: React.ComponentType<PageProps>) => (
           fields.radiationTreatments = []
         }
 
-        const otherFiles = (fields.otherFiles || []).map((file, index) => ({
-          ...file,
-          title: file.title || `Файл №${index + 1}`,
-        }))
+        const otherFiles = (fields.otherFiles || [])
+          .filter(file => !!file.url)
+          .map((file, index) => ({
+            ...file,
+            title: file.title || `Файл №${index + 1}`,
+          }))
 
         return { ...fields, id, otherFiles }
       }
