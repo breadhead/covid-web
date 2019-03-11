b379a3023bafa17d10784b734542d8a4bc0c2535
Igor Kamyshev <igor@kamyshev.me>
2019-03-11 16:27:30 +0300

    feat: fix so long filename stirngs

--- features/common/uploader/displayFileName.ts
+++ features/common/uploader/displayFileName.ts
@@ -1,8 +1,20 @@
+const MAX_LENGTH = 19
+const BACKLASH = 5
+
 export const displayFileName = (fileName: string) => {
   const underscorePosition = fileName.indexOf('_')
 
   const position =
     underscorePosition === -1 ? fileName.length - 16 : underscorePosition
 
-  return fileName.slice(position + 1)
+  const realFileName = fileName.slice(position + 1)
+
+  if (realFileName.length < MAX_LENGTH) {
+    return realFileName
+  }
+
+  const start = realFileName.slice(0, MAX_LENGTH - BACKLASH)
+  const end = realFileName.slice(realFileName.length - BACKLASH)
+
+  return `${start}...${end}`
 }
