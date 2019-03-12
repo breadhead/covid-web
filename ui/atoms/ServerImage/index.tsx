a5b53c4fe7ee36b5b63750ed8c0640b218ebfc58
Igor Kamyshev <igor@kamyshev.me>
2019-03-12 15:18:46 +0300

    fix(admin): fix server image path

--- ui/atoms/ServerImage/index.tsx
+++ ui/atoms/ServerImage/index.tsx
@@ -1,10 +1,17 @@
+import { useMemo } from 'react'
+
 type Props = React.DetailedHTMLProps<
   React.ImgHTMLAttributes<HTMLImageElement>,
   HTMLImageElement
 >
 
-const ServerImage = ({ src, ...rest }: Props) => (
-  <img src={`${src}`} {...rest} />
-)
+const ServerImage = ({ src, ...rest }: Props) => {
+  const path = useMemo(
+    () => (src && src.startsWith('http') ? src : `//${src}`),
+    [src],
+  )
+
+  return <img src={path} {...rest} />
+}
 
 export default ServerImage
