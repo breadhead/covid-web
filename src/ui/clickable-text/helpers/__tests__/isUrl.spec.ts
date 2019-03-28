d97bf7a7990bb85995a91303df6159b45b38ce0c
iwanttobealight <iwanttobealight@yandex.ru>
2019-03-28 16:31:48 +0300

    fix(is url): fix regexp

--- src/ui/clickable-text/helpers/__tests__/isUrl.spec.ts
+++ src/ui/clickable-text/helpers/__tests__/isUrl.spec.ts
@@ -44,4 +44,22 @@ describe('isUrl', () => {
 
     expect(isUrl(text)).toBeFalsy()
   })
+
+  test('should return false for email with long domain', () => {
+    const text = 'd.sidelnikova@nenaprasno.tennis'
+
+    expect(isUrl(text)).toBeFalsy()
+  })
+
+  test('should return false for email with underscore', () => {
+    const text = 'd_sidelnikova@nenaprasno.tennis'
+
+    expect(isUrl(text)).toBeFalsy()
+  })
+
+  test('should return false for email with +', () => {
+    const text = 'd_sidelnikova+12@nenaprasno.tennis'
+
+    expect(isUrl(text)).toBeFalsy()
+  })
 })
--- src/ui/clickable-text/helpers/isUrl.ts
+++ src/ui/clickable-text/helpers/isUrl.ts
@@ -2,7 +2,11 @@ import { EMAIL_REGEXP } from './EMAIL_REGEXP'
 import { URL_REGEXP } from './URL_REGEXP'
 
 export const isUrl = (text: string): boolean => {
-  const match = !text.match(EMAIL_REGEXP) && text.match(URL_REGEXP)
+  if (text.match(EMAIL_REGEXP)) {
+    return false
+  }
+
+  const match = text.match(URL_REGEXP)
 
   if (!match) {
     return false
