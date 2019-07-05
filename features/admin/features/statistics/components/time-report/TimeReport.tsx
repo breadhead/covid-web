8e4f5f53c3bb9bd720654fab0f4c1935f7bbb911
iwanttobealight <iwanttobealight@yandex.ru>
2019-07-05 14:48:55 +0300

    refactor(admin/time-report): put all logic in one file

--- features/admin/features/statistics/components/time-report/TimeReport.tsx
+++ features/admin/features/statistics/components/time-report/TimeReport.tsx
@@ -1,9 +1,91 @@
-import React, { useState, useEffect } from 'react'
-import { Table } from 'antd'
+import React, { useState, useEffect, useRef } from 'react'
+import { Table, Input, Button, Icon } from 'antd'
 import { useApi } from '@app/lib/api/useApi'
 import { formatTimestamp } from './formatTimestamp'
 import { TimeReport as TimeReportModel } from '@app/src/domain/statistics/model/time-report'
-import { useColumnSearchProps } from './useColumnSearchProps'
+import Highlighter from 'react-highlight-words'
+/* eslint-disable react/display-name */
+
+interface Props {
+  setSelectedKeys: (evt: any) => void
+  selectedKeys: any
+  confirm: () => void
+  clearFilters: () => void
+}
+
+const useColumnSearchProps = () => {
+  const [searchText, setSearchText] = useState('')
+  const searchInput = useRef(null)
+
+  const handleSearch = (selectedKeys: string, confirm: () => void) => {
+    confirm()
+    setSearchText(selectedKeys[0])
+  }
+
+  const handleReset = (clearFilters: any) => {
+    clearFilters()
+    setSearchText('')
+  }
+
+  const getColumnSearchProps = (dataIndex: any) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }: Props) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: (filtered: string) => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value: string, record: string) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    render: (text: string) => (
+      <Highlighter
+        highlightStyle={{
+          backgroundColor: 'var(--color-highlight)',
+          padding: 0,
+        }}
+        searchWords={[searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  })
+
+  return getColumnSearchProps
+}
 
 export const TimeReport = () => {
   const api = useApi()
