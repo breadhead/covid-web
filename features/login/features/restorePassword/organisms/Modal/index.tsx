21817edadf47447689986ffa09bd5c05c82b98f4
iwanttobealight <iwanttobealight@yandex.ru>
2019-07-17 11:18:16 +0300

    refactor(restore-form): move restore-form to single component

--- features/login/features/restorePassword/organisms/Modal/index.tsx
+++ features/login/features/restorePassword/organisms/Modal/index.tsx
@@ -1,11 +1,7 @@
-import { InputType } from '@app/features/common/form'
-import Form from '@app/features/common/form/components/Form/Form'
-import Input from '@app/features/common/form/components/Input'
-import { Button, ButtonKind } from '@front/ui/button'
 import * as React from 'react'
-import { schema } from '../../container'
 import * as styles from './RestorePasswordModal.css'
 import ModalFooter from '@app/features/login/organisms/Footer'
+import { RestoreForm } from '../restore-form'
 
 interface Props {
   onFormSubmit: () => Promise<any>
@@ -21,29 +17,9 @@ const RestorePasswordModal = ({
   openSignUp,
   phone,
 }: Props) => {
-  console.log('phone:', phone)
   return (
     <article className={styles.popup}>
-      <h1 className={styles.title}>Забыли пароль?</h1>
-      <button className={styles.actionButton} onClick={openSignUp}>
-        Зарегистрироваться
-      </button>
-      <Form onSubmit={onFormSubmit}>
-        {() => (
-          <>
-            <Input
-              className={styles.input}
-              name='login'
-              label={'Введите вашу почту'}
-              type={InputType.Text}
-              validate={schema.login}
-            />
-            <Button submit className={styles.mainButton}>
-              Сбросить пароль
-            </Button>
-          </>
-        )}
-      </Form>
+      <RestoreForm onFormSubmit={onFormSubmit} openSignUp={openSignUp} />
       <ModalFooter onOpenModalClick={openSignIn} />
     </article>
   )
---
+++ features/login/features/restorePassword/organisms/restore-form/RestoreForm.tsx
@@ -0,0 +1,39 @@
+import { InputType } from '@app/features/common/form'
+import Form from '@app/features/common/form/components/Form/Form'
+import Input from '@app/features/common/form/components/Input'
+import { Button } from '@front/ui/button'
+import * as React from 'react'
+import { schema } from '../../container'
+import * as styles from './RestoreForm.css'
+
+interface Props {
+  openSignUp: () => void
+  onFormSubmit: () => void
+}
+
+export const RestoreForm = ({ openSignUp, onFormSubmit }: Props) => {
+  return (
+    <>
+      <h1 className={styles.title}>Забыли пароль?</h1>
+      <button className={styles.actionButton} onClick={openSignUp}>
+        Зарегистрироваться
+      </button>
+      <Form onSubmit={onFormSubmit}>
+        {() => (
+          <>
+            <Input
+              className={styles.input}
+              name="login"
+              label={'Введите вашу почту'}
+              type={InputType.Text}
+              validate={schema.login}
+            />
+            <Button submit className={styles.mainButton}>
+              Сбросить пароль
+            </Button>
+          </>
+        )}
+      </Form>
+    </>
+  )
+}
