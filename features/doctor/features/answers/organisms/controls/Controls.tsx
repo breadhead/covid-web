9716fa8d12bca5b94efeb1d8cce0b6fc2eef719b
iwanttobealight <iwanttobealight@yandex.ru>
2019-07-23 18:41:43 +0300

    fix(doctors-controls): fix draft controls visibility

--- features/doctor/features/answers/organisms/controls/Controls.tsx
+++ features/doctor/features/answers/organisms/controls/Controls.tsx
@@ -3,6 +3,9 @@ import * as React from 'react'
 import Router from 'next/router'
 import { ButtonKind, ButtonWithTooltip } from '@app/features/common/form'
 import { Button } from '@front/ui/button'
+import { useMappedState } from 'redux-react-hook'
+import { getClaimStatus } from '@app/features/common/consultation'
+import ClaimStatus from '@app/models/Claim/ClaimStatus'
 
 interface Props {
   styles: { [key: string]: string }
@@ -21,41 +24,51 @@ export const Controls = ({
   answerSent,
   id,
 }: Props) => {
+  const currentClaimStatus = useMappedState(getClaimStatus)
+  const draftControlsCondition = currentClaimStatus === ClaimStatus.AtTheDoctor
+
   return (
-    <div className={styles.controls}>
-      {isEditMode && (
-        <Button
-          className={styles.cancel}
-          kind={ButtonKind.Secondary}
-          onClick={() => Router.push(`/consultation/redirect/${id}`) as any}
-        >
-          Отменить изменения
-        </Button>
-      )}
-      <ButtonWithTooltip
-        className={styles.save}
-        submit
-        onClick={() => setPreAnswer(false)}
-      >
-        {answerSent ? 'Сохранить изменения' : 'Отправить ответ'}
-      </ButtonWithTooltip>
-      {!isEditMode && (
-        <Button kind={ButtonKind.Secondary} onClick={() => setEditMode(true)}>
-          Редактировать
-        </Button>
-      )}
-      {isEditMode && (
-        <ButtonWithTooltip
-          className={styles.draft}
-          kind={ButtonKind.Secondary}
-          submit
-          onClick={() => {
-            setPreAnswer(true)
-          }}
-        >
-          Сохранить как черновик
-        </ButtonWithTooltip>
-      )}
-    </div>
+    <>
+      {draftControlsCondition ? (
+        <div className={styles.controls}>
+          {isEditMode && (
+            <Button
+              className={styles.cancel}
+              kind={ButtonKind.Secondary}
+              onClick={() => Router.push(`/consultation/redirect/${id}`) as any}
+            >
+              Отменить изменения
+            </Button>
+          )}
+          <ButtonWithTooltip
+            className={styles.save}
+            submit
+            onClick={() => setPreAnswer(false)}
+          >
+            {answerSent ? 'Сохранить изменения' : 'Отправить ответ'}
+          </ButtonWithTooltip>
+          {!isEditMode && (
+            <Button
+              kind={ButtonKind.Secondary}
+              onClick={() => setEditMode(true)}
+            >
+              Редактировать
+            </Button>
+          )}
+          {isEditMode && (
+            <ButtonWithTooltip
+              className={styles.draft}
+              kind={ButtonKind.Secondary}
+              submit
+              onClick={() => {
+                setPreAnswer(true)
+              }}
+            >
+              Сохранить как черновик
+            </ButtonWithTooltip>
+          )}
+        </div>
+      ) : null}
+    </>
   )
 }
