c24c409e0923981c9de0540a37e33b4f8e6820d6
iwanttobealight <iwanttobealight@yandex.ru>
2019-08-13 12:04:55 +0300

    feat(utm-codes): delete extra utm-codes

--- features/client/features/consultation/molecules/NotificationButton/components/ClosedButton/index.tsx
+++ features/client/features/consultation/molecules/NotificationButton/components/ClosedButton/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 
 import { Button } from '@front/ui/button'
 import { NavLink } from '@front/ui/nav-link'
-import { expertAnswersUTM } from '@app/features/common/analytics/utmCodes'
 
 interface Props {
   claimId: string
@@ -11,7 +10,7 @@ interface Props {
 
 const ClosedButton = ({ claimId, className }: Props) => (
   <NavLink
-    href={`/client/consultation/${claimId}?${expertAnswersUTM}#expert-answers`}
+    href={`/client/consultation/${claimId}#expert-answers`}
     withoutUnderline
     className={className}
   >
