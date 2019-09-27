2bf142074d51eb56d9c29dd00252b40ac4d92fa6
Daria Moreno-Gogoleva <iwanttobealight@yandex.ru>
2019-09-27 11:29:41 +0300

    Update URL_REGEXP.ts

--- src/ui/clickable-text/helpers/URL_REGEXP.ts
+++ src/ui/clickable-text/helpers/URL_REGEXP.ts
@@ -3,4 +3,4 @@
 // Just use, it's correct
 export const URL_REGEXP = /(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))?/g
 
-export const URL_FILE_REGEX = /((\/|\\|\/\/|https?:\\\\|https?:\/\/)[a-z0-9\s_@\-^!#$%&+={}.\/\\\[\]]+)+\.zip|rar|7z|tar|zipx|DMG$/
+export const URL_FILE_REGEX = /((\/|\\|\/\/|https?:\\\\|https?:\/\/)[a-z0-9\s_@\-^!#$%&+={}.\/\\\[\]]+)+\.zip|rar|7z|tar|zipx|pdf|DMG$/
