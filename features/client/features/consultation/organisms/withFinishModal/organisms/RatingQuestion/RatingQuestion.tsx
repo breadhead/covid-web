523cd50d54f4cf8da4080c9e52fb62885945c566
iwanttobealight <iwanttobealight@yandex.ru>
2019-10-17 19:16:03 +0300

    feat(rating question): finish base logic

--- features/client/features/consultation/organisms/withFinishModal/organisms/RatingQuestion/RatingQuestion.tsx
+++ features/client/features/consultation/organisms/withFinishModal/organisms/RatingQuestion/RatingQuestion.tsx
@@ -3,8 +3,6 @@ import { useState, useMemo, useCallback } from 'react'
 import { buttons } from './config/buttons'
 
 import * as s from './RatingQuestion.css'
-import { questions } from './config/questions'
-import { keyBy } from 'lodash'
 import { RatingButton } from '../../molecules/RatingButton'
 import { NextQuestionButton } from '../../molecules/NextQuestionButton'
 import {
@@ -24,12 +22,11 @@ interface RatingQuestionProps {
 
 export const RatingQuestion = React.memo(
   ({ submit, error, claimId, questions }: RatingQuestionProps) => {
-    const [questionId, setQuestionId] = useState<RatingQuestionsEnum>(
-      RatingQuestionsEnum.Q1,
-    )
+    const [questionId, setQuestionId] = useState<number>(DEFAULT_QUESTION_ID)
     const [rating, setRating] = useState<number>(DEFAULT_RATING_VALUE)
 
-    const currentQuestion = useMemo(() => keyBy(questions, 'id')[questionId], [
+    const currentQuestion = useMemo(() => questions[questionId], [
+      questions,
       questionId,
     ])
 
@@ -38,27 +35,31 @@ export const RatingQuestion = React.memo(
     const submitRatingQuestion = async () => {
       const data = {
         claimId,
-        question: currentQuestion.id,
+        question: currentQuestion.id as RatingQuestionsEnum,
         answer: `${rating}`,
       }
 
-      // await submit(data).then(() => {
-      //   const newId = questionId + 1
-      //   if (newId > questions.length) {
-      //     setQuestionId(DEFAULT_QUESTION_ID)
-      //     return
-      //   }
-      //   setQuestionId(newId)
-      //   resetRating()
-      // })
+      await submit(data)
+        .then(() => {
+          const newId = questionId + 1
+          if (newId >= questions.length) {
+            //TODO: add new q
+
+            setQuestionId(DEFAULT_QUESTION_ID)
+            return
+          }
+          setQuestionId(newId)
+          resetRating()
+        })
+        .catch(e => console.log('error', e))
     }
-    console.log('questions:', questions)
+
     return questions.length > 0 ? (
       <>
-        {/* <p className={s.text}>
-          {currentQuestion.id}. {currentQuestion.question}
+        <p className={s.text}>
+          {questionId + 1}. {currentQuestion.question}
         </p>
-        <p className={s.hint}>{currentQuestion.hint}</p> */}
+        <p className={s.hint}>{currentQuestion.hint}</p>
         <div className={s.buttonsContainer}>
           {buttons.map(btn => (
             <RatingButton key={btn.id} button={btn} setRating={setRating} />
--- features/client/features/consultation/organisms/withFinishModal/organisms/RatingQuestion/config/defaultValues.ts
+++ features/client/features/consultation/organisms/withFinishModal/organisms/RatingQuestion/config/defaultValues.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_QUESTION_ID = 1
+export const DEFAULT_QUESTION_ID = 0
 export const DEFAULT_RATING_VALUE = 0
