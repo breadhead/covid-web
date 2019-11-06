47a4dd36c7e885b5858d664ec1c2a395898f9fac
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-06 16:12:21 +0300

    feat(stories): move fetching

--- features/admin/features/story/components/StoriesTable/StoriesTable.tsx
+++ features/admin/features/story/components/StoriesTable/StoriesTable.tsx
@@ -3,26 +3,13 @@ import { Table } from 'antd'
 
 import { useStoriesColumns } from './useStoriesColumns'
 
-import { useThunk } from '@app/src/hooks/useThunk';
-import { useEffect } from 'react';
-import { fetchStories } from '@app/features/admin/domain';
 import { useMappedState } from 'redux-react-hook';
 import { selectStories } from '@app/features/admin/domain/selectors/selectStories';
 
 export const StoriesTable = () => {
-  const dispatch = useThunk()
-
-  useEffect(() => {
-    const fetch = async () => {
-      await dispatch(fetchStories())
-    }
-
-    fetch()
-  }, [])
-
 
   const stories = useMappedState(selectStories)
   const columns = useStoriesColumns()
 
-  return !!stories && <Table dataSource={stories} columns={columns} />
+  return !!stories ? <Table dataSource={stories} columns={columns} /> : null
 }
