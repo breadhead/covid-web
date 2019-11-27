9fee522867991f5bb9039156cf969eed78de3040
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-27 16:37:12 +0300

    feat(statistics): add rating-doctors

---
+++ features/admin/features/statistics/components/rating-doctors/RatingDoctors.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+export const RatingDoctors = () => {
+  return (<div>doctors rating here</div>);
+}
+ 
---
+++ features/admin/features/statistics/components/rating-doctors/index.ts
@@ -0,0 +1,1 @@
+export { RatingDoctors } from './RatingDoctors'
