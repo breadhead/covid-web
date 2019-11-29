327d424f4026e6b4cc36af56eca5996833e01048
iwanttobealight <iwanttobealight@yandex.ru>
2019-11-29 12:30:28 +0300

    feat(detail table): add content

--- features/admin/features/statistics/components/rating-doctors/components/detail-table/DetailTable.tsx
+++ features/admin/features/statistics/components/rating-doctors/components/detail-table/DetailTable.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
 import { Button, ButtonKind } from '@app/src/ui/button';
+import { RatingDoctorsType } from '../../../../RatingDoctors';
+import * as s from './DetailTable.css'
 
 interface DetailTableProps {
   setCurrent: (value: null) => void
+  content: RatingDoctorsType
 }
 
-export const DetailTable = ({ setCurrent }: DetailTableProps) => {
-  return (<div>
+export const DetailTable = ({ setCurrent, content }: DetailTableProps) => {
 
+  const { doctor, average, value, comment } = content
+
+
+  return (<div>
     <Button
       kind={ButtonKind.Secondary}
       onClick={() => {
         setCurrent(null)
       }}>Ко всем врачам</Button>
 
-    detail table
+    {!!content && <section className={s.content}>
+      <h1>{doctor}</h1>
+      <span>Средний рейтинг по всем вопросам: {average}</span>
+
+    </section>}
   </div>)
 }
