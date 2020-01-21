b7acb0fb576c1000ce2b26f4b9c596ac05bf7a0c
iwanttobealight <iwanttobealight@yandex.ru>
2020-01-21 11:42:06 +0300

    feat(stats): move doctors tabls closer

--- features/admin/features/statistics/Statistics.tsx
+++ features/admin/features/statistics/Statistics.tsx
@@ -8,9 +8,9 @@ import { ClaimArchive } from './components/claim-archive'
 import { TimeReport } from './components/time-report'
 import s from './Statistics.css'
 import { ClaimsFunnel } from './components/funnel'
-import { Rating } from './components/rating'
-import { Comments } from './components/comments'
+
 import { RatingDoctors } from './components/rating-doctors'
+import { RatingConsultations } from './components/rating-consultations';
 
 export const Statistics = () => {
   return (
@@ -24,30 +24,13 @@ export const Statistics = () => {
         <TabPane tab="Время ответа врачей" key="time" className={s.tab}>
           <TimeReport />
         </TabPane>
-        <TabPane tab="Воронка продаж" key="funnel" className={s.tab}>
-          <ClaimsFunnel />
-        </TabPane>
-        <TabPane
-          tab="Оценка качества консультаций"
-          key="rating"
-          className={s.tab}
-        >
-          <Tabs defaultActiveKey="rating-questions">
-            <TabPane tab="Рейтинг" key="rating-questions" className={s.tab}>
-              <Rating />
-            </TabPane>
-            <TabPane
-              tab="Комментарии"
-              key="comment-questions"
-              className={s.tab}
-            >
-              <Comments />
-            </TabPane>
-          </Tabs>
-        </TabPane>
         <TabPane tab="Рейтинг врачей" key="rating-doctors" className={s.tab}>
           <RatingDoctors />
         </TabPane>
+        <TabPane tab="Воронка продаж" key="funnel" className={s.tab}>
+          <ClaimsFunnel />
+        </TabPane>
+        <RatingConsultations />
       </Tabs>
     </Layout>
   )
---
+++ features/admin/features/statistics/components/rating-consultations/index.ts
@@ -0,0 +1,1 @@
+export { RatingConsultations } from './RatingConsultations'
