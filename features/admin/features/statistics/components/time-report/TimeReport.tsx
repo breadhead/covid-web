0fd24687996d26790bd45f0c0887be7587fee98f
iwanttobealight <iwanttobealight@yandex.ru>
2020-01-21 14:11:43 +0300

    feat(time report): move calendar to single component

--- features/admin/features/statistics/components/time-report/TimeReport.tsx
+++ features/admin/features/statistics/components/time-report/TimeReport.tsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Table } from 'antd'
 import { useApi } from '@app/lib/api/useApi'
 import { formatTimestamp } from '../../helpers/formatTimestamp'
 import { TimeReport as TimeReportModel } from '@app/src/domain/statistics/model/time-report'
 import { useColumnSearchProps } from './useColumnSearchProps'
-import RangePicker from '../../../history/molecule/RangePicker'
-import { DEFAULT_START } from '../funnel/ClaimsFunnel'
 import { useCurrentColumns } from './useCurrentColumns'
-import { Button } from '@app/src/ui/button'
-import saveFile from 'js-file-download'
-import formatDate from '@app/features/client/features/claims/helpers/formatDate'
+import { DEFAULT_START } from '../funnel/ClaimsFunnel';
+import { ReportCalendar } from './components/report-calendar';
+import { getTableData } from './helpers/getTableData';
+
+const now = new Date()
 
 export const TimeReport = () => {
   const api = useApi()
-  const now = new Date()
   const [timeData, setTimeData] = useState<TimeReportModel | null>(null)
+
   const [from, setFrom] = useState<Date>(DEFAULT_START)
   const [to, setTo] = useState<Date>(now)
 
@@ -28,46 +28,13 @@ export const TimeReport = () => {
   const getColumnSearchProps = useColumnSearchProps()
   const columns = useCurrentColumns(getColumnSearchProps)
 
-  const changePeriod = useCallback(
-    (dates: [Date, Date] | undefined) => {
-      if (!dates) {
-        setFrom(DEFAULT_START)
-        setTo(now)
-      } else {
-        const [newFrom, newTo] = dates
-        setFrom(newFrom)
-        setTo(newTo)
-      }
-    },
-    [setFrom],
-  )
-
-  const downloadFile = useCallback(
-    async () => {
-      if (!from || !to) {
-        return
-      }
-
-      const file = await api.fetchTimeReportTable(from, to)
-      saveFile(
-        file,
-        `Отчёт времени работы врачей ${formatDate(from)}-${formatDate(to)}.csv`,
-      )
-    },
-    [from, to],
-  )
   if (!timeData) {
     return <p>Загружаем...</p>
   }
 
   const { median, max, average, doctors, success, failure } = timeData
 
-  const tableData =
-    doctors &&
-    doctors.map(doctor => ({
-      key: doctor.name,
-      ...doctor,
-    }))
+  const tableData = getTableData(doctors)
 
   return (
     <div>
@@ -79,17 +46,7 @@ export const TimeReport = () => {
           <p>Всего заявок, закрытых вовремя: {success}</p>
           <p>Всего просроченных заявок: {failure}</p>
         </div>
-        <div style={{ zIndex: 10 }}>
-          <RangePicker
-            dateIsDisabled={date => date < DEFAULT_START || date > now}
-            onChange={changePeriod}
-          />
-          {!!from && !!to && (
-            <div style={{ marginTop: '20px' }}>
-              <Button onClick={downloadFile}>Скачать отчёт</Button>
-            </div>
-          )}
-        </div>
+        <ReportCalendar from={from} to={to} setFrom={setFrom} setTo={setTo} />
       </section>
       <Table columns={columns} dataSource={tableData} />
     </div>
---
+++ features/admin/features/statistics/components/time-report/helpers/getTableData.ts
@@ -0,0 +1,7 @@
+export const getTableData = (doctors: any[]) => {
+  return doctors &&
+    doctors.map(doctor => ({
+      key: doctor.name,
+      ...doctor,
+    }))
+}
