11be9b46ed314e981980f93adf21a7d8d2940015
iwanttobealight <iwanttobealight@yandex.ru>
2020-01-28 18:13:52 +0300

    feat(comments): add pagination

--- features/admin/features/statistics/components/rating-doctors/components/detail-comments/DetailComments.tsx
+++ features/admin/features/statistics/components/rating-doctors/components/detail-comments/DetailComments.tsx
@@ -1,19 +1,58 @@
 import * as React from 'react'
+import { useState, useCallback } from 'react'
 import { RatingCommentQuestion } from '../../../../RatingCommentQuestion'
+import { CommentsButttons } from './components/buttons'
+
+const STEP = 20
 
 interface DetailCommentsProps {
   comments: RatingCommentQuestion[]
 }
 
-export const DetailComments = ({ comments: comment }: DetailCommentsProps) => {
+export const DetailComments = ({ comments }: DetailCommentsProps) => {
+  const [index, setIndex] = useState(STEP)
+  const [items, setItems] = useState(comments.slice(0, STEP))
+
+  const show = useCallback(
+    (side: 'next' | 'prev') => {
+      let newIndex
+      let currentItems
+
+      switch (side) {
+        case 'next':
+          newIndex = index + STEP < comments.length ? index + STEP : index
+          currentItems = comments.slice(index, newIndex)
+          break
+        case 'prev':
+          newIndex = index - STEP > 0 ? index - STEP : index
+          const prevIndex = newIndex - STEP
+          currentItems = comments.slice(prevIndex, newIndex)
+          break
+        default:
+          newIndex = index
+          currentItems = items
+          break
+      }
+
+      setItems(currentItems)
+      setIndex(newIndex)
+    },
+    [index],
+  )
+
   return (
     <>
-      {comment
+      <CommentsButttons
+        show={show}
+        nextCondition={index + 20 <= comments.length}
+        prevCondition={index - STEP > 0}
+      />
+      {items
         .filter(it => it.text.length > 0)
         .map((com, key) => {
           return (
             <div key={com.claimId}>
-              <a href={`/manager/consultation/${com.claimId}`} target="_blank">
+              <a href={`/manager/consultation/${com.claimId}`} rel="noopener noreferrer" target="_blank">
                 <span>Перейти к заявке</span>
               </a>
               <p>
@@ -22,6 +61,11 @@ export const DetailComments = ({ comments: comment }: DetailCommentsProps) => {
             </div>
           )
         })}
+      <CommentsButttons
+        show={show}
+        nextCondition={index + 20 <= comments.length}
+        prevCondition={index - STEP > 0}
+      />
     </>
   )
 }
