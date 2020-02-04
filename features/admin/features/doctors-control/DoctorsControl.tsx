67cf97ca6c1f604c66faf11a019e6b01d18624df
iwanttobealight <iwanttobealight@yandex.ru>
2020-02-04 12:24:13 +0300

    feat(doctors control): move add-doctor-form to single component

--- features/admin/features/doctors-control/DoctorsControl.tsx
+++ features/admin/features/doctors-control/DoctorsControl.tsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
-import { Form, Input, InputType, TextArea } from '@app/features/common/form'
-import { Button, ButtonKind } from '@app/src/ui/button'
+
 import * as s from './DoctorsControl.css'
 
 import Layout from '../../organisms/Layout'
 import { useApi } from '@app/lib/api/useApi'
 import { CreateDoctorRequest } from '@app/lib/api/request/CreateDoctorRequest'
 import { User } from '@app/models/Users/User'
-import schema from './validation'
+import { AddDoctorForm } from './components/add-doctor-form'
 
 export const DoctorsControl = () => {
   const [doctor, setDoctor] = useState<User | null>(null)
   const [password, setPassword] = useState<string>('')
   const [emptyPassword, setEmptyPassword] = useState<boolean>(false)
-  const [error, setError] = useState<string>('')
+
   const api = useApi()
 
   const createDoctor = (data: any) => {
@@ -32,7 +31,7 @@ export const DoctorsControl = () => {
     api
       .createDoctor(finalData as CreateDoctorRequest)
       .then(setDoctor)
-      .catch(error => setError(error.message))
+      .catch(error => console.log('create doctor error', error))
   }
 
   const generatePassword = () => {
@@ -47,13 +46,6 @@ export const DoctorsControl = () => {
     <>
       <Layout>
         <div className={s.doctorsControl}>
-          {!!error && (
-            <p>
-              Ошибка! {error} <br />
-              Попробуйте ещё раз
-            </p>
-          )}
-
           {!!doctor && (
             <div className={s.success}>
               <h2>Врач добавлен</h2>
@@ -64,71 +56,12 @@ export const DoctorsControl = () => {
               </p>
             </div>
           )}
-
-          <h1>Добавить врача</h1>
-          <Form onSubmit={createDoctor} resetAfterSubmit>
-            {() => (
-              <>
-                <Input
-                  validate={schema.name}
-                  name="name"
-                  type={InputType.Text}
-                  label="Фамилия"
-                />
-                <br />
-                <TextArea
-                  name="description"
-                  type={InputType.Text}
-                  label="Описание"
-                />
-                <br />
-                <Input
-                  validate={schema.email}
-                  name="email"
-                  type={InputType.Text}
-                  label="Email"
-                />
-                <br />
-                <Input
-                  validate={schema.boardUsername}
-                  name="boardUsername"
-                  type={InputType.Text}
-                  label="Трелло id (без @)"
-                />
-                <br />
-                <Input
-                  name="telegramId"
-                  type={InputType.Text}
-                  label="Телеграм id"
-                />
-                <br />
-                <div className={s.password}>
-                  <Input
-                    name="rawPassword"
-                    type={InputType.Text}
-                    label="Пароль"
-                    value={password}
-                    readOnly
-                  />
-                  <Button
-                    kind={ButtonKind.Secondary}
-                    className={s.buttonGenerate}
-                    onClick={generatePassword}
-                  >
-                    сгенерировать
-                  </Button>
-                </div>
-                {!!emptyPassword ? (
-                  <div className={s.passwordError}>
-                    Обязательное поле. Нажмите кнопку "сгенерировать"
-                  </div>
-                ) : null}
-                <Button className={s.button} submit>
-                  Добавить
-                </Button>
-              </>
-            )}
-          </Form>
+          <AddDoctorForm
+            password={password}
+            emptyPassword={emptyPassword}
+            createDoctor={createDoctor}
+            generatePassword={generatePassword}
+          />
         </div>
       </Layout>
     </>
---
+++ features/admin/features/doctors-control/components/add-doctor-form/AddDoctorForm.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react'
+import { Form, Input, InputType, TextArea } from '@app/features/common/form'
+import { Button, ButtonKind } from '@app/src/ui/button'
+
+import * as s from './../../DoctorsControl.css'
+
+import schema from './../../validation'
+
+interface AddDoctorFormProps {
+  createDoctor: (data: any) => void
+  generatePassword: () => void
+  password: string
+  emptyPassword: boolean
+}
+
+export const AddDoctorForm = ({
+  createDoctor,
+  generatePassword,
+  password,
+  emptyPassword,
+}: AddDoctorFormProps) => {
+  return (
+    <>
+      <h1>Добавить врача</h1>
+      <Form onSubmit={createDoctor} resetAfterSubmit>
+        {() => (
+          <>
+            <Input
+              validate={schema.name}
+              name="name"
+              type={InputType.Text}
+              label="Фамилия"
+            />
+            <br />
+            <TextArea
+              name="description"
+              type={InputType.Text}
+              label="Описание"
+            />
+            <br />
+            <Input
+              validate={schema.email}
+              name="email"
+              type={InputType.Text}
+              label="Email"
+            />
+            <br />
+            <Input
+              validate={schema.boardUsername}
+              name="boardUsername"
+              type={InputType.Text}
+              label="Трелло id (без @)"
+            />
+            <br />
+            <Input
+              name="telegramId"
+              type={InputType.Text}
+              label="Телеграм id"
+            />
+            <br />
+            <div className={s.password}>
+              <Input
+                name="rawPassword"
+                type={InputType.Text}
+                label="Пароль"
+                value={password}
+                readOnly
+              />
+              <Button
+                kind={ButtonKind.Secondary}
+                className={s.buttonGenerate}
+                onClick={generatePassword}
+              >
+                сгенерировать
+              </Button>
+            </div>
+            {!!emptyPassword ? (
+              <div className={s.passwordError}>
+                Обязательное поле. Нажмите кнопку "сгенерировать"
+              </div>
+            ) : null}
+            <Button className={s.button} submit>
+              Добавить
+            </Button>
+          </>
+        )}
+      </Form>
+    </>
+  )
+}
