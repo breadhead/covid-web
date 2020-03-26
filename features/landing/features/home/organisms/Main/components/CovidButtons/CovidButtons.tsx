c6b029ef7cf0b590c57e1e6b5eda8a9d236839b2
iwanttobealight <iwanttobealight@yandex.ru>
2020-03-26 17:50:08 +0300

    feat(covid buttons): add links

--- features/landing/features/home/organisms/Main/components/CovidButtons/CovidButtons.tsx
+++ features/landing/features/home/organisms/Main/components/CovidButtons/CovidButtons.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import * as styles from './CovidButtons.css';
 import { Button, ButtonSize } from '@app/src/ui/button';
+import { NavLink } from '@app/src/ui/nav-link';
 
 
 export const CovidButtons = () => {
   return (
     <div className={styles.buttons}>
-      <Button size={ButtonSize.ExtraLarge}>Полный справочник по COVID-19</Button>
+      <NavLink withoutUnderline blank href="http://faq.defeatcovid.ru/ru/articles/3830701-%D0%B8%D0%BD%D1%81%D1%82%D1%80%D1%83%D0%BA%D1%86%D0%B8%D1%8F-%D0%BF%D0%BE-%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D0%B0%D0%BA%D1%82%D0%B8%D0%BA%D0%B5-covid-2019">
+        <Button size={ButtonSize.ExtraLarge}>Полный справочник по COVID-19</Button>
+      </NavLink>
+      <NavLink withoutUnderline href="/request">
       <Button size={ButtonSize.ExtraLarge}>Персональная консультация</Button>
+      </NavLink>
     </div>
   )
 }
