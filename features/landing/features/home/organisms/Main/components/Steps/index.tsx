407ca1f3a644683c8d3e271806a68310aa7c411d
iwanttobealight <iwanttobealight@yandex.ru>
2020-03-26 16:16:05 +0300

    feat(steps): update styles

--- features/landing/features/home/organisms/Main/components/Steps/index.tsx
+++ features/landing/features/home/organisms/Main/components/Steps/index.tsx
@@ -7,11 +7,15 @@ import Step from './Step'
 import { steps } from './config'
 
 const Steps = () => (
-  <section className={styles.steps}>
-    {steps.map(step => (
-      <Step key={step.index} step={step} />
-    ))}
-  </section>
+  <div className={styles.wrap}>
+    <h2 className={styles.title}>Как работает персональная 
+    консультация</h2>
+    <section className={styles.steps}>
+      {steps.map(step => (
+        <Step key={step.index} step={step} />
+      ))}
+    </section>
+  </div>
 )
 
 export default Steps
