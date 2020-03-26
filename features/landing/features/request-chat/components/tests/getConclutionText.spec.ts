f4448138dd6cb899df7af0b0c99ee9c952ceae24
iwanttobealight <iwanttobealight@yandex.ru>
2020-03-26 19:55:33 +0300

    feat(conclution): add text logic

---
+++ features/landing/features/request-chat/components/tests/getConclutionText.spec.ts
@@ -0,0 +1,11 @@
+import { getConclutionText } from '../../getConclutionText'
+
+
+describe('getConclutionText', () => {
+  test('should return false for non-link text', () => {
+    const text = 'Hello world!'
+
+    expect(getConclutionText(text)).toBeFalsy()
+  })
+
+})
