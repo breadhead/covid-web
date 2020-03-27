b4e17ea6b150ed03f30d73d842c94c7fcc80bcda
iwanttobealight <iwanttobealight@yandex.ru>
2020-03-27 13:33:40 +0300

    feat(intro): fix mobile styles

--- ui/organisms/Footer/components/Login/index.tsx
+++ ui/organisms/Footer/components/Login/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 
-import cx from 'classnames'
-
 import styles from './Login.css'
 
-import StartConsultationButton from '@app/features/landing/features/home/molecules/StartConsultationButton'
 import LoginButton from '@app/features/main/layout/organisms/Header/atoms/LoginButton'
 
 interface Props {
@@ -14,11 +11,6 @@ interface Props {
 const Login = ({ showLoginButton }: Props) => (
   <div className={styles.buttons}>
     {showLoginButton && <LoginButton className={styles.button} />}
-    <StartConsultationButton
-      className={cx(styles.button, styles.startConsultationButton)}
-    >
-      Начать консультацию
-    </StartConsultationButton>
   </div>
 )
 
