05c47c80f44a9df5a153592451b74a88887a5d2e
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-03 13:14:34 +0300

    add footer

--- features/system/layout/footer/SystemFooter.tsx
+++ features/system/layout/footer/SystemFooter.tsx
@@ -1,7 +1,47 @@
 import * as React from 'react';
 
+import { NavLink } from '@app/src/ui/nav-link';
+import Legal from '@app/ui/organisms/Footer/components/Legal';
+import { footerProjects } from '@app/ui/organisms/Footer/components/Partners/footerProjects';
+
+import * as styles from './SystemFooter.css';
+
 // interface SystemFooterProps {}
 
 export const SystemFooter = () => {
-  return <div>footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <nav className={styles.navigation}>
+        <NavLink withoutUnderline href="/experts" className={styles.link}>
+          Эксперты
+        </NavLink>
+        <NavLink withoutUnderline href="/partners" className={styles.link}>
+          Партнеры
+        </NavLink>
+        <NavLink withoutUnderline href="/reports" className={styles.link}>
+          Отчёты
+        </NavLink>
+        <NavLink withoutUnderline href="/contacts" className={styles.link}>
+          Контакты
+        </NavLink>
+      </nav>
+
+      <div className={styles.projects}>
+        {footerProjects.map(({ id, text, logo, href }) => (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            key={id}
+            href={href}
+            className={styles.project}
+          >
+            <p className={styles.text}>{text}</p>
+            <img className={styles.logo} src={logo} alt={text} />
+          </a>
+        ))}
+      </div>
+
+      <Legal className={styles.legal} />
+    </footer>
+  );
 };
