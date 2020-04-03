4a5db45dcb9146724a6929bcbe7a31173c17d52d
uncleseneca <uncleseneca@gmail.com>
2020-04-03 13:24:16 +0300

    dont pass dimensions to images

--- lib/useImageSrc/getImageProxySrc.ts
+++ lib/useImageSrc/getImageProxySrc.ts
@@ -3,4 +3,4 @@ import base64 from 'base-64';
 import { getFromConfig } from '../getPublicRuntimeConfig';
 
 export const getImageProxySrc = (url: string) =>
-  `${getFromConfig('storageUrl')}/pr:desktop/${base64.encode(url)}`;
+  `${getFromConfig('storageUrl')}/w:0/h:0/${base64.encode(url)}`;
