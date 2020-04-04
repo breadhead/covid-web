5d768608d8d1995d44152c5c273b68a1aefe6a54
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-04 12:01:20 +0300

    system help: add mobile styles

--- features/system/main/components/help/SystemHelp.tsx
+++ features/system/main/components/help/SystemHelp.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import * as styles from './SystemHelp.css';
 import { HelpOptions } from './components/options';
 import { HelpFooter } from './components/help-footer';
-// interface SystemHelpProps {}
 
 export const SystemHelp = () => {
   return (
