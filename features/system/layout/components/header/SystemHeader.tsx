031cf9d1149db0ce196ade9cf958518c16449276
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-04 10:02:02 +0300

    system-header: add menu

--- features/system/layout/components/header/SystemHeader.tsx
+++ features/system/layout/components/header/SystemHeader.tsx
@@ -3,9 +3,11 @@ import * as React from 'react';
 import { Icon } from '@app/src/ui/icon';
 import { IconsList } from '@app/src/ui/sprite';
 import { NavLink } from '@app/src/ui/nav-link';
+import MediaQuery, { Query } from '@app/ui/MediaQuery';
 
 import * as styles from './SystemHeader.css';
 import { SystemNavigation } from './navigation';
+import { SystemMobileMenu } from './system-mobile-menu';
 
 // interface SystemHeaderProps {}
 
@@ -15,7 +17,13 @@ export const SystemHeader = () => {
       <NavLink className={styles.iconWrapper} withoutUnderline href="/">
         <Icon name={IconsList.SystemLogo} />
       </NavLink>
-      <SystemNavigation />
+
+      <MediaQuery className={styles.buttonContainer} query={Query.ToExtraLarge}>
+        <SystemMobileMenu />
+      </MediaQuery>
+      <MediaQuery className={styles.navContainer} query={Query.FromExtraLarge}>
+        <SystemNavigation />
+      </MediaQuery>
     </header>
   );
 };
---
+++ features/system/layout/components/header/system-mobile-menu/SystemMobileMenu.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+// interface SystemMobileMenuProps {}
+
+export const SystemMobileMenu = () => {
+  return <div>SystemMobileMenu</div>;
+};
