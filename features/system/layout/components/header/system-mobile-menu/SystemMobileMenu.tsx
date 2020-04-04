0e21665d3a4d4f6a6678d6cefb4f47858dae70c3
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-04 10:19:49 +0300

    mobile menu: add burger and overlay

--- features/system/layout/components/header/system-mobile-menu/SystemMobileMenu.tsx
+++ features/system/layout/components/header/system-mobile-menu/SystemMobileMenu.tsx
@@ -1,7 +1,29 @@
-import * as React from 'react';
+import React, { useState, useCallback } from 'react';
+
+import Overlay from '@app/ui/Overlay';
+import BurgerButton from '@app/features/main/layout/organisms/Header/atoms/BurgerButton';
 
 // interface SystemMobileMenuProps {}
 
 export const SystemMobileMenu = () => {
-  return <div>SystemMobileMenu</div>;
+  const [menuOpened, setOpened] = useState(false);
+
+  const show = useCallback(() => setOpened(true), []);
+  const hide = useCallback(() => setOpened(false), []);
+  return (
+    <div>
+      <Overlay isVisible={menuOpened} onClick={hide} />
+      <BurgerButton show={show} />
+      menu t=items menu t=items menu t=items menu t=items menu t=items menu
+      t=items menu t=items menu t=items menu t=items menu t=items menu t=items
+      menu t=items menu t=items menu t=items menu t=items menu t=items menu
+      t=items menu t=items
+      {/* <Menu
+        showLoginButton={showLoginButton}
+        signOut={signOut}
+        hide={hide}
+        isVisible={menuOpened}
+      /> */}
+    </div>
+  );
 };
