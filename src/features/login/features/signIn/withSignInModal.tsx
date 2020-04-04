e533d6802fe9996c5185ed36ab3ce605961b0234
uncleseneca <uncleseneca@gmail.com>
2020-04-04 18:47:43 +0300

    fix cyclic import

--- src/features/login/features/signIn/withSignInModal.tsx
+++ src/features/login/features/signIn/withSignInModal.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { withModal, WithModalProps } from '@app/src/features/common/modal';
+import { useModal } from '@app/src/features/common/modal';
 import { SIGN_IN_MODAL } from '@app/src/features/login';
 
 export interface WithSignInModal {
   openSignIn: () => void;
 }
 
-const Container = (Component: React.ComponentType<WithSignInModal>) =>
-  withModal(({ modal, ...props }: WithModalProps) => (
-    <Component {...props} openSignIn={() => modal.open(SIGN_IN_MODAL)} />
-  ));
+const Container = (Component: React.ComponentType<WithSignInModal>) => (
+  props,
+) => {
+  const { open, close } = useModal();
+
+  return (
+    <Component
+      {...props}
+      close={close}
+      openSignIn={() => open(SIGN_IN_MODAL)}
+    />
+  );
+};
 
 export default Container;
