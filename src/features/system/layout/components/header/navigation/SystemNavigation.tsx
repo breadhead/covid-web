2d46cfdc00396de93be4bdfee1ba7897622c6a1c
uncleseneca <uncleseneca@gmail.com>
2020-04-04 15:19:35 +0300

    add active state to header

--- src/features/system/layout/components/header/navigation/SystemNavigation.tsx
+++ src/features/system/layout/components/header/navigation/SystemNavigation.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import cx from 'classnames';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { Icon } from '@app/src/ui/icon';
 import { NavLink } from '@app/src/ui/nav-link';
@@ -12,58 +14,83 @@ interface Props {
   hide?: () => void;
 }
 
-export const SystemNavigation = ({ className, hide }: Props) => (
-  <div className={cx(styles.menu, className)}>
-    <button className={styles.closeButton} onClick={hide}>
-      закрыть меню
-      <Icon className={styles.NavIcon} name={IconsList.CloseLight} />
-    </button>
-    <div className={styles.mainMenu}>
-      <NavLink
-        withoutUnderline
-        href="/ask"
-        className={cx(styles.mainLink, styles.link)}
-      >
-        Справочная служба
-      </NavLink>
-      <NavLink
-        withoutUnderline
-        href="/doctors"
-        className={cx(styles.mainLink, styles.link)}
-      >
-        Врачам
-      </NavLink>
-      <NavLink
-        withoutUnderline
-        href="/for-clinics"
-        className={cx(styles.mainLink, styles.link)}
-      >
-        Помощь больницам
-      </NavLink>
-    </div>
+export const SystemNavigation = ({ className, hide }: Props) => {
+  const { asPath } = useRouter();
+  const router = useRouter();
+  console.log('SystemNavigation -> router', router);
+  return (
+    <div className={cx(styles.menu, className)}>
+      <button className={styles.closeButton} onClick={hide}>
+        закрыть меню
+        <Icon className={styles.NavIcon} name={IconsList.CloseLight} />
+      </button>
 
-    <div className={styles.contentMenu}>
-      <NavLink
-        withoutUnderline
-        href="/experts"
-        className={cx(styles.contentLink, styles.link)}
-      >
-        Эксперты
-      </NavLink>
-      <NavLink
-        withoutUnderline
-        href="/partners"
-        className={cx(styles.contentLink, styles.link)}
-      >
-        Партнеры
-      </NavLink>
-      <NavLink
-        withoutUnderline
-        href="/reports"
-        className={cx(styles.contentLink, styles.link)}
-      >
-        Отчёты
-      </NavLink>
+      <div className={styles.mainMenu}>
+        {mainLinks.map((link) => (
+          <NavLink
+            key={link.href}
+            withoutUnderline
+            href={link.href}
+            className={cx(
+              link.className,
+              asPath === link.href && styles.active,
+            )}
+          >
+            {link.text}
+          </NavLink>
+        ))}
+      </div>
+
+      <div className={styles.contentMenu}>
+        {contentLinks.map((link) => (
+          <NavLink
+            key={link.href}
+            withoutUnderline
+            href={link.href}
+            className={cx(
+              link.className,
+              asPath === link.href && styles.active,
+            )}
+          >
+            {link.text}
+          </NavLink>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+const mainLinks = [
+  {
+    href: '/ask',
+    text: 'Справочная служба',
+    className: cx(styles.mainLink, styles.link),
+  },
+  {
+    href: '/doctors',
+    text: 'Врачам',
+    className: cx(styles.mainLink, styles.link),
+  },
+  {
+    href: '/for-clinics',
+    text: 'Помощь больницам',
+    className: cx(styles.mainLink, styles.link),
+  },
+];
+const contentLinks = [
+  {
+    href: '/experts',
+    text: 'Эксперты',
+    className: cx(styles.contentLink, styles.link),
+  },
+  {
+    href: '/partners',
+    text: 'Партнеры',
+    className: cx(styles.contentLink, styles.link),
+  },
+  {
+    href: '/reports',
+    text: 'Отчёты',
+    className: cx(styles.contentLink, styles.link),
+  },
+];
---
+++ src/lib/routing/RouteType.ts
@@ -0,0 +1,13 @@
+export enum RouteType {
+  landing = '/landing',
+  landingRequest = '/landing/request',
+  landingVolunteer = '/landing/volunteer',
+  landingHelp = '/landing/help',
+  landingHospitalAid = '/landing/hospital-aid',
+  landingRequestChat = '/landing/request-chat',
+  landingPartners = '/landing/partners',
+  landingContacts = '/landing/contacts',
+  landingExperts = '/landing/experts',
+  landingExpert = '/landing/expert',
+  landingForClinics = '/landing/for-clinics',
+}
