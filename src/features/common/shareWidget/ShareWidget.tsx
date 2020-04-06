15c7352c2d47cb46b8c404d618fc5a7f0352655a
Kate <katya@breadhead.ru>
2020-04-06 14:28:24 +0700

    add share widget

---
+++ src/features/common/shareWidget/ShareWidget.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+import * as styles from './ShareWidget.css';
+import { IconsList } from '@app/src/ui/sprite';
+
+import {Icon} from "@front/ui/icon";
+import {
+    FacebookShareButton,
+    OKShareButton,
+    TelegramShareButton,
+    VKShareButton
+} from "react-share";
+
+interface Props {
+    shareUrl: string;
+}
+
+export const ShareWidget = ({ shareUrl }: Props) => {
+    const title = 'Пусть больше людей узнает о проекте';
+    return (
+        <footer className={styles.helpFooter}>
+            <p className={styles.text}>
+                {title}
+                <span className={styles.textSecondary}>
+                  Поделитесь с друзьями и коллегами. Вместе победим! 💪
+                </span>
+            </p>
+            <nav className={styles.social}>
+                <FacebookShareButton url={shareUrl} quote={title} className={styles.iconWrapper} >
+                    <Icon className={styles.icon} name={IconsList.Facebook} />
+                </FacebookShareButton>
+                <VKShareButton url={shareUrl} className={styles.iconWrapper}>
+                    <Icon className={styles.icon} name={IconsList.Vk} />
+                </VKShareButton>
+                <TelegramShareButton url={shareUrl} title={title} lassName={styles.iconWrapper}>
+                    <Icon className={styles.icon} name={IconsList.Telegram} />
+                </TelegramShareButton>
+                <OKShareButton url={shareUrl} title={title} className={styles.iconWrapper}>
+                    <Icon className={styles.icon} name={IconsList.OK} />
+                </OKShareButton>
+            </nav>
+        </footer>
+    )
+};
--- src/features/system/main/components/help/SystemHelp.tsx
+++ src/features/system/main/components/help/SystemHelp.tsx
@@ -3,13 +3,15 @@ import * as React from 'react';
 import * as styles from './SystemHelp.css';
 import { HelpOptions } from './components/options';
 import { HelpFooter } from './components/help-footer';
+import {ShareWidget} from "@front/features/common/shareWidget";
+import {getFromConfig} from "@front/lib/getPublicRuntimeConfig";
 
 export const SystemHelp = () => {
   return (
     <section className={styles.systemHelp}>
       <h2 className="gl-sectionTitle">Как помочь</h2>
       <HelpOptions />
-      <HelpFooter />
+      <ShareWidget shareUrl={getFromConfig('prodUrl')} />
     </section>
   );
 };
