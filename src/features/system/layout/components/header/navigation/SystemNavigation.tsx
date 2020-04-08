86d980cb03ce6c0cd6763687d0d94674710dd17e
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-08 14:18:25 +0300

    header: fix links jumping

--- src/features/system/layout/components/header/navigation/SystemNavigation.tsx
+++ src/features/system/layout/components/header/navigation/SystemNavigation.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import cx from 'classnames';
 import { useRouter } from 'next/router';
 
-import { useWindowSize } from '@app/src/lib/window-size';
-
 import * as styles from './SystemNavigationContainer.css';
 import { LinksList } from './linksList';
 import { mainLinks, contentLinks } from '../links';
@@ -11,12 +9,10 @@ import { mainLinks, contentLinks } from '../links';
 interface Props {
   narrow?: boolean;
   className?: string;
-  hide?: () => void;
 }
 
-export const SystemNavigation = ({ className, hide, narrow }: Props) => {
+export const SystemNavigation = ({ className, narrow }: Props) => {
   const { asPath } = useRouter();
-  const { width } = useWindowSize();
 
   return (
     <div className={cx(styles.menu, className)}>
@@ -25,7 +21,6 @@ export const SystemNavigation = ({ className, hide, narrow }: Props) => {
         className={cx(styles.menu, styles.mainMenu)}
         asPath={asPath}
         narrow={narrow}
-        width={width}
         linkClassName={cx(styles.mainLink, styles.link)}
       >
         {mainLinks}
@@ -36,7 +31,6 @@ export const SystemNavigation = ({ className, hide, narrow }: Props) => {
         className={cx(styles.menu, styles.contentMenu)}
         asPath={asPath}
         narrow={narrow}
-        width={width}
         linkClassName={cx(styles.contentLink, styles.link)}
       >
         {contentLinks}
