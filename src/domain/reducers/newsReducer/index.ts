48dc7571f71c7b078dfc8b07d253b031238c9d61
uncleseneca <uncleseneca@gmail.com>
2020-04-09 13:08:57 +0300

    add main page news and sorting

--- src/domain/reducers/newsReducer/index.ts
+++ src/domain/reducers/newsReducer/index.ts
@@ -3,15 +3,18 @@ import { combineReducers } from 'redux';
 import * as newsItem from './item';
 import * as newsList from './list';
 import * as featuredNewsList from './featured';
+import * as mainPageList from './mainPageList';
 
 export interface State {
   list: newsList.State;
   item: newsItem.State;
   featuredNews: featuredNewsList.State;
+  mainPageList: mainPageList.State;
 }
 
 export const reducer = combineReducers<State, any>({
   item: newsItem.reducer,
   list: newsList.reducer,
   featuredNews: featuredNewsList.reducer,
+  mainPageList: mainPageList.reducer,
 } as any);
--- src/features/landing/features/news/featuredNews/FeaturedNews.tsx
+++ src/features/landing/features/news/featuredNews/FeaturedNews.tsx
@@ -4,6 +4,7 @@ import { useMappedState } from 'redux-react-hook';
 
 import { selectFeaturedNews } from '@app/src/domain/reducers/newsReducer/featured/selectFeaturedNews';
 import { NavLink } from '@app/src/ui/nav-link';
+import { NewsItem } from '@app/src/domain/models/common/NewsItem';
 
 import s from './FeaturedNews.css';
 import { NewsCard } from '../newsCard';
@@ -11,18 +12,19 @@ import { NewsCard } from '../newsCard';
 interface FeaturedNewsProps {
   title?: string;
   className?: string;
+  newsList: NewsItem[];
 }
 
 export const FeaturedNews = ({
   title = 'Другие новости',
   className,
+  newsList,
 }: FeaturedNewsProps) => {
-  const featuredNews = useMappedState(selectFeaturedNews);
   return (
     <div className={cx(s.wrapper, className)}>
       <h2 className={cx(s.title, 'gl-sectionTitle')}>{title}</h2>
       <div className={s.list}>
-        {featuredNews.map((newsItem) => (
+        {newsList.map((newsItem) => (
           <NewsCard key={newsItem._id} data={newsItem}></NewsCard>
         ))}
       </div>
