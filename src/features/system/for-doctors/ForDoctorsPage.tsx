66b5b7b9a4a2c1548c446cf81c3ffe4472396b27
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-09 12:19:59 +0300

    for-doctors: add base layout

--- src/features/system/for-doctors/ForDoctorsPage.tsx
+++ src/features/system/for-doctors/ForDoctorsPage.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
 
+import { SystemLayout } from '../layout';
+
 export const ForDoctorsPage = () => {
-  return <div>for doctors page</div>;
+  return (
+    <SystemLayout>
+      <section className="gl-wrapper gl-section">
+        <h1 className="gl-pageTitle">Врачам</h1>
+      </section>
+    </SystemLayout>
+  );
 };
