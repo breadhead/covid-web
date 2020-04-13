ef23354c241cde94984b523262b84d1eddacd179
iwanttobealight <iwanttobealight@yandex.ru>
2020-04-13 11:40:32 +0300

    supervisor page: add serializer

--- src/features/system/expert-board/features/Expert/page/index.tsx
+++ src/features/system/expert-board/features/Expert/page/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Head from 'next/head';
+import BlockContent from '@sanity/block-content-to-react';
 
 import { SystemLayout } from '@app/src/features/system/layout';
 import { Expert as ExpertModel } from '@app/src/domain/models/common/Expert';
 import { getImageSrc } from '@app/src/lib/useImageSrc/getImageSrc';
 import { NavLink } from '@app/src/ui/nav-link';
+import { getSerializer } from '@app/src/features/common/getSerializer/getSerializer';
 
 import * as styles from './ExpertPage.css';
 
@@ -13,6 +15,8 @@ interface Props {
 }
 
 const SupervisorPage = ({ expert }: Props) => {
+  const serializers = getSerializer({});
+
   return (
     <>
       <Head>
@@ -33,11 +37,10 @@ const SupervisorPage = ({ expert }: Props) => {
               <h1 className={styles.title}>{expert.name}</h1>
               <p className={styles.description}>{expert.subtitle}</p>
               <div className={styles.info}>
-                {expert.description.map((blob) =>
-                  blob.children.map((child) => (
-                    <p key={child._key}>{child.text}</p>
-                  )),
-                )}
+                <BlockContent
+                  blocks={expert.description}
+                  serializers={serializers}
+                />
               </div>
             </div>
           </section>
