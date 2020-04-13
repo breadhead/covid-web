2a59fe439212f09970ef347d079d1f37cedf1d08
uncleseneca <uncleseneca@gmail.com>
2020-04-13 15:33:55 +0300

    add filtering tags by category

--- src/domain/reducers/tagsReducer/selectTags.ts
+++ src/domain/reducers/tagsReducer/selectTags.ts
@@ -1,11 +1,22 @@
 import { State } from '@app/src/lib/store';
 
+import { ALL_CATEGORIES } from '../../models/common/NewsCategoryType';
+
 export enum TagsType {
   News = 'newsCount',
   Articles = 'articlesCount',
 }
 
-export const selectTags = (tagsType: TagsType) => (state: State) =>
-  state.tags.list.filter((item) => item[tagsType] > 0);
+export const selectTags = (tagsType: TagsType, category: string) => (
+  state: State,
+) => {
+  const nonEmptyTagsByType = state.tags.list.filter(
+    (item) => item[tagsType] > 0,
+  );
+
+  if (category === ALL_CATEGORIES) return nonEmptyTagsByType;
+
+  return nonEmptyTagsByType.filter((tag) => (tag.count[category] || 0) > 0);
+};
 
 export const selectAllTags = (state: State) => state.tags.list;
