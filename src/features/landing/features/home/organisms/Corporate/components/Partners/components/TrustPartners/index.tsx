3c884a7bec5c1500861847b44a1de66d4d8c3ef1
leonid <leon@thatsme.ru>
2020-04-24 22:28:11 +0300

    found styling stage 1

--- src/features/landing/features/home/organisms/Corporate/components/Partners/components/TrustPartners/index.tsx
+++ src/features/landing/features/home/organisms/Corporate/components/Partners/components/TrustPartners/index.tsx
@@ -7,13 +7,24 @@ import { NavLink } from '@app/src/ui/nav-link';
 import { getImageSrc } from '@front/lib/useImageSrc/getImageSrc';
 
 import * as styles from './TrustPartners.css';
+import {Icon} from "@front/ui/icon";
+import {IconsList} from "@front/ui/sprite";
 
 const TrustPartners = () => {
   const partners = useMappedState(selectTrustPartners);
 
   return partners.length > 0 ? (
-    <div>
-      <section>
+    <div className={styles.container}>
+      <NavLink
+        className={styles.logoWrapper}
+        withoutUnderline
+        href="https://nenaprasno.ru"
+        blank
+      >
+        <Icon className={styles.mainLogo} name={IconsList.FoundationLogo} />
+      </NavLink>
+      <div className={styles.plus}/>
+      <section className={styles.scrollContainer}>
         {partners.map(({ name, logo, url }) => (
           <NavLink
             className={styles.logoWrapper}
@@ -24,7 +35,7 @@ const TrustPartners = () => {
           >
             <img
               className={styles.logo}
-              src={getImageSrc(logo, 0, 56) || ''}
+              src={getImageSrc(logo, 0, 112) || ''}
               alt={name}
             />
           </NavLink>
