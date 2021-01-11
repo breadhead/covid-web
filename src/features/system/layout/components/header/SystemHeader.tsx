bd8d33c0be8854a8c4e6dc26ab10b41226dffd05
leonid <leon@thatsme.ru>
2021-01-11 13:27:59 +0300

    remove header notice

--- src/features/system/layout/components/header/SystemHeader.tsx
+++ src/features/system/layout/components/header/SystemHeader.tsx
@@ -36,13 +36,6 @@ export const SystemHeader = ({ white, fixed, show }: SystemHeaderProps) => {
 
   return (
     <>
-      {!fixed && (<div className={styles.headerNotice}>
-        <div className={styles.text}>
-          {
-            'Мы понимаем, что вы переживаете за свое здоровье и здоровье своих близких. В новогодние праздники — с 31 декабря по 8 января — время ответа специалиста может увеличиться. Если вы чувствуете, что вам необходима экстренная консультация, пожалуйста, не дожидайтесь ответа эксперта и срочно обратитесь к врачу или вызовите скорую помощь.'
-          }
-        </div>
-      </div>)}
       <SystemMobileMenu hide={hideMenu} menuOpened={menuOpened} />
       <header
         className={cx(
